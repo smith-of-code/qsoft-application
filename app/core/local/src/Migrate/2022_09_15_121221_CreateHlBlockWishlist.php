9f108cecb10665ef9c3f07b4858e82eafbdae838
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-15 12:26:30 +0300

    225620 / Добавил миграцию для сущности избранных товаров

---
+++ app/core/local/src/Migrate/2022_09_15_121221_CreateHlBlockWishlist.php
@@ -0,0 +1,37 @@
+<?php
+
+use QSoft\Migrate\BaseCreateHighloadMigration;
+
+class CreateHlBlockWishlist extends BaseCreateHighloadMigration
+{
+    protected array $blockInfo = [
+        'NAME'       => 'HlWishlist',
+        'TABLE_NAME' => 'wishlist',
+    ];
+
+    protected array $blockLang = [
+        'LID' => 'ru',
+        'NAME' => 'HL-блок избранных товаров',
+    ];
+
+    protected array $fields = [
+        [
+            'FIELD_NAME' => 'UF_USER_ID',
+            'USER_TYPE_ID' => 'integer',
+            'XML_ID' => 'UF_USER_ID',
+            'SORT' => 100,
+            'EDIT_FORM_LABEL' => ['ru' => 'Идентификатор пользователя'],
+            'LIST_COLUMN_LABEL' => ['ru' => 'Идентификатор пользователя'],
+            'LIST_FILTER_LABEL' => ['ru' => 'Идентификатор пользователя'],
+        ],
+        [
+            'FIELD_NAME' => 'UF_PRODUCT_ID',
+            'USER_TYPE_ID' => 'integer',
+            'XML_ID' => 'UF_PRODUCT_ID',
+            'SORT' => 100,
+            'EDIT_FORM_LABEL' => ['ru' => 'Идентификатор товара'],
+            'LIST_COLUMN_LABEL' => ['ru' => 'Идентификатор товара'],
+            'LIST_FILTER_LABEL' => ['ru' => 'Идентификатор товара'],
+        ],
+    ];
+}
