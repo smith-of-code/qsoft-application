b563ee039528995a2451810b820ea3feecbe6f12
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-15 13:20:49 +0300

    225620 / Добавил миграцию для сопутствующих продуктов

---
+++ app/core/local/src/Migrate/2022_09_15_131621_CreateHlBlockRelatedProduct.php
@@ -0,0 +1,37 @@
+<?php
+
+use QSoft\Migrate\BaseCreateHighloadMigration;
+
+class CreateHlBlockSlider extends BaseCreateHighloadMigration
+{
+    protected array $blockInfo = [
+        'NAME'       => 'HlRelatedProduct',
+        'TABLE_NAME' => 'related_product',
+    ];
+
+    protected array $blockLang = [
+        'LID' => 'ru',
+        'NAME' => 'HL-блок сопустствующих товаров',
+    ];
+
+    protected array $fields = [
+        [
+            'FIELD_NAME' => 'UF_MAIN_PRODUCT_ID',
+            'USER_TYPE_ID' => 'integer',
+            'XML_ID' => 'UF_MAIN_PRODUCT_ID',
+            'SORT' => 100,
+            'EDIT_FORM_LABEL' => ['ru' => 'Заголовок основного товара'],
+            'LIST_COLUMN_LABEL' => ['ru' => 'Заголовок основного товара'],
+            'LIST_FILTER_LABEL' => ['ru' => 'Заголовок основного товара'],
+        ],
+        [
+            'FIELD_NAME' => 'UF_RELATED_PRODUCT_ID',
+            'USER_TYPE_ID' => 'integer',
+            'XML_ID' => 'UF_RELATED_PRODUCT_ID',
+            'SORT' => 100,
+            'EDIT_FORM_LABEL' => ['ru' => 'Заголовок сопутствующего товара'],
+            'LIST_COLUMN_LABEL' => ['ru' => 'Заголовок сопутствующего товара'],
+            'LIST_FILTER_LABEL' => ['ru' => 'Заголовок сопутствующего товара'],
+        ],
+    ];
+}
