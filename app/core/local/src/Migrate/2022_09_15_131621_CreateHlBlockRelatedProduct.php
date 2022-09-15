f4ae99b752748fa53c413ed1632ad885477a2643
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-15 13:21:32 +0300

    225620 / Изменил название класса миграции

--- app/core/local/src/Migrate/2022_09_15_131621_CreateHlBlockRelatedProduct.php
+++ app/core/local/src/Migrate/2022_09_15_131621_CreateHlBlockRelatedProduct.php
@@ -2,7 +2,7 @@
 
 use QSoft\Migrate\BaseCreateHighloadMigration;
 
-class CreateHlBlockSlider extends BaseCreateHighloadMigration
+class CreateHlBlockRelatedProduct extends BaseCreateHighloadMigration
 {
     protected array $blockInfo = [
         'NAME'       => 'HlRelatedProduct',
