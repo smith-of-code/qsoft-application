3ea5312ac6c8fb8d2dd147f8a4a1a0af1332e614
Мотин Михаил Сергеевич <motin.m_qschool@qsoft.ru>
2022-09-15 18:28:07 +0300

    225620 / Добавил ORM списка желаемого

---
+++ app/core/local/src/ORM/lang/ru/WishlistTable.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['WISHLIST_ENTITY_ID_FIELD'] = 'ID';
+$MESS['WISHLIST_ENTITY_UF_USER_ID_FIELD'] = 'Идентификатор пользователя';
+$MESS['WISHLIST_ENTITY_UF_PRODUCT_ID_FIELD'] = 'Идентификатор товара';
