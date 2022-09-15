71a37d28a595cb7eb9c2923606eb9952ad33a969
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-15 12:35:55 +0300

    225620 / Добавил миграцию для сущности слайдеров

---
+++ app/core/local/src/Migrate/2022_09_15_123421_CreateHlBlockSlider.php
@@ -0,0 +1,28 @@
+<?php
+
+use QSoft\Migrate\BaseCreateHighloadMigration;
+
+class CreateHlBlockSlider extends BaseCreateHighloadMigration
+{
+    protected array $blockInfo = [
+        'NAME'       => 'HlSlider',
+        'TABLE_NAME' => 'slider',
+    ];
+
+    protected array $blockLang = [
+        'LID' => 'ru',
+        'NAME' => 'HL-блок слайдеров',
+    ];
+
+    protected array $fields = [
+        [
+            'FIELD_NAME' => 'UF_TITLE',
+            'USER_TYPE_ID' => 'string',
+            'XML_ID' => 'UF_TITLE',
+            'SORT' => 100,
+            'EDIT_FORM_LABEL' => ['ru' => 'Заголовок'],
+            'LIST_COLUMN_LABEL' => ['ru' => 'Заголовок'],
+            'LIST_FILTER_LABEL' => ['ru' => 'Заголовок'],
+        ],
+    ];
+}
