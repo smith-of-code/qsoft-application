b8870acca31ce340787b912e97508a1e853732e5
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-15 13:47:16 +0300

    225620 / Добавил модель ORM для групп пользователей

---
+++ app/core/local/src/ORM/GroupPropertiesTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace QSoft\ORM;
+
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Entity;
+
+Loc::loadMessages(__FILE__);
+
+class GroupPropertiesTable extends Entity\DataManager
+{
+    public static function getTableName(): string
+    {
+        return 'team';
+    }
+
+    public static function getMap(): array
+    {
+        return [
+            new Entity\IntegerField('ID', [
+                'primary' => true,
+                'autocomplete' => true,
+                'title' => Loc::getMessage('TEAM_ENTITY_ID_FIELD'),
+            ]),
+            new Entity\IntegerField('UF_GROUP_ID', [
+                'required' => true,
+                'title' => Loc::getMessage('TEAM_ENTITY_UF_GROUP_ID_FIELD'),
+            ]),
+            new Entity\IntegerField('UF_DISCOUNT', [
+                'required' => true,
+                'title' => Loc::getMessage('TEAM_ENTITY_UF_DISCOUNT_FIELD'),
+            ]),
+        ];
+    }
+}
