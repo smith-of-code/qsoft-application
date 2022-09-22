9805adb06cfa622f83534fbd99406f4ad63c28ef
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-22 14:50:32 +0300

    226086 / Добавил кэш и режим совместимости

--- app/core/local/components/zolo/catalog.element/templates/.default/lang/ru/.parameters.php
+++ app/core/local/components/zolo/catalog.element/templates/.default/lang/ru/.parameters.php
@@ -6,3 +6,4 @@ $MESS['PARAMETER_ELEMENT_ID_NAME'] = 'ID элемента';
 $MESS['PARAMETER_ELEMENT_CODE_NAME'] = 'Символьный код элемента';
 $MESS['PARAMETER_SECTION_ID_NAME'] = 'ID раздела';
 $MESS['PARAMETER_SECTION_CODE_NAME'] = 'Символьный код раздела';
+$MESS['PARAMETER_COMPATIBLE_MODE_NAME'] = 'Режим совместимости';
