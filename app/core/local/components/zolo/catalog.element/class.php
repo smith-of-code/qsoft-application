e9118f387d42436044681fc010007cb7db56680c
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-22 15:50:27 +0300

    226086 / Поменял родительский класс и поменял название переменной

--- app/core/local/components/zolo/catalog.element/class.php
+++ app/core/local/components/zolo/catalog.element/class.php
@@ -2,7 +2,6 @@
 
 use Bitrix\Main;
 use	Bitrix\Main\Loader;
-use	Bitrix\Iblock\Component\Element;
 use	Bitrix\Main\Localization\Loc;
 use	Bitrix\Catalog;
 use Bitrix\Iblock\Component\Tools;
@@ -19,205 +18,44 @@ if (!Loader::includeModule('iblock'))
 	return;
 }
 
-class CatalogElementComponent extends Element
+class CatalogElementComponent extends CBitrixComponent
 {
-	public function __construct($component = null)
-	{
-		parent::__construct($component);
-		$this->setExtendedMode(false);
-	}
-
 	/**
-	 * Processing parameters unique to catalog.element component.
-	 *
-	 * @param array $params		Component parameters.
+	 * @param array $arParams
 	 * @return array
 	 */
-	public function onPrepareComponentParams($params)
+	public function onPrepareComponentParams($arParams): array
 	{
-		$params = parent::onPrepareComponentParams($params);
+		$arParams = parent::onPrepareComponentParams($arParams);
 
-		$params['COMPATIBLE_MODE'] = (isset($params['COMPATIBLE_MODE']) && $params['COMPATIBLE_MODE'] === 'N' ? 'N' : 'Y');
-
-		$this->setCompatibleMode($params['COMPATIBLE_MODE'] === 'Y');
-
-        $params['IBLOCK_TYPE'] = trim($params['IBLOCK_TYPE'] ?? '');
-        if (!$params['IBLOCK_TYPE']) {
+        $arParams['IBLOCK_TYPE'] = trim($arParams['IBLOCK_TYPE'] ?? '');
+        if (!$arParams['IBLOCK_TYPE']) {
             Tools::process404(Loc::getMessage('IBLOCK_TYPE_NOT_SET'), false, false);
-            $params['.ERROR'] = true;
+            $arParams['.ERROR'] = true;
 
-            return $params;
+            return $arParams;
         }
 
-        $params['IBLOCK_ID'] = (int)(trim($params['IBLOCK_ID']) ?? 0);
-        if (!$params['IBLOCK_ID']) {
+        $arParams['IBLOCK_ID'] = (int)(trim($arParams['IBLOCK_ID']) ?? 0);
+        if (!$arParams['IBLOCK_ID']) {
             Tools::process404(Loc::getMessage('IBLOCK_ID_NOT_SET'), false, false);
-            $params['.ERROR'] = true;
+            $arParams['.ERROR'] = true;
 
-            return $params;
+            return $arParams;
         }
 
-        $params['ELEMENT_ID'] = (int)(trim($params['ELEMENT_ID']) ?? 0);
-        $params['ELEMENT_CODE'] = trim($params['ELEMENT_CODE'] ?? '');
-        if (!$params['ELEMENT_ID'] && !$params['ELEMENT_CODE']) {
+        $arParams['ELEMENT_ID'] = (int)(trim($arParams['ELEMENT_ID']) ?? 0);
+        $arParams['ELEMENT_CODE'] = trim($arParams['ELEMENT_CODE'] ?? '');
+        if (!$arParams['ELEMENT_ID'] && !$arParams['ELEMENT_CODE']) {
             Tools::process404(Loc::getMessage('ELEMENT_DATA_NOT_SET'), false, false);
-            $params['.ERROR'] = true;
+            $arParams['.ERROR'] = true;
 
-            return $params;
+            return $arParams;
         }
 
-        $params['SECTION_ID'] = (int)(trim($params['SECTION_ID']) ?? 0);
-        $params['SECTION_CODE'] = trim($params['SECTION_CODE'] ?? '');
+        $arParams['SECTION_ID'] = (int)(trim($arParams['SECTION_ID']) ?? 0);
+        $arParams['SECTION_CODE'] = trim($arParams['SECTION_CODE'] ?? '');
 
-		return $params;
-	}
-
-	/**
-	 * Fill additional keys for component cache.
-	 *
-	 * @param array &$resultCacheKeys		Cached result keys.
-	 * @return void
-	 */
-	protected function initAdditionalCacheKeys(&$resultCacheKeys)
-	{
-		parent::initAdditionalCacheKeys($resultCacheKeys);
-
-		if (
-			$this->useCatalog
-			&& !empty($this->storage['CATALOGS'][$this->arParams['IBLOCK_ID']])
-			&& is_array($this->storage['CATALOGS'][$this->arParams['IBLOCK_ID']])
-		)
-		{
-			$element =& $this->elements[0];
-
-			// catalog hit stats
-			$productTitle = !empty($element['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])
-				? $element['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
-				: $element['NAME'];
-
-			$categoryId = '';
-			$categoryPath = array();
-
-			if (isset($element['SECTION']['ID']))
-			{
-				$categoryId = $element['SECTION']['ID'];
-			}
-
-			if (isset($element['SECTION']['PATH']))
-			{
-				foreach ($element['SECTION']['PATH'] as $cat)
-				{
-					$categoryPath[$cat['ID']] = $cat['NAME'];
-				}
-			}
-
-			$this->arResult['CATEGORY_PATH'] = implode('/', $categoryPath);
-
-			$counterData = array(
-				'product_id' => $element['ID'],
-				'iblock_id' => $this->arParams['IBLOCK_ID'],
-				'product_title' => $productTitle,
-				'category_id' => $categoryId,
-				'category' => $categoryPath
-			);
-
-			if (empty($element['OFFERS']))
-			{
-				$priceProductId = $element['ID'];
-			}
-			else
-			{
-				$offer = reset($element['OFFERS']);
-				$priceProductId = $offer['ID'];
-				unset($offer);
-			}
-
-			// price for anonymous
-			if ($this->useDiscountCache)
-			{
-				if ($this->storage['USE_SALE_DISCOUNTS'])
-				{
-					$priceTypes = array();
-					$priceIterator = Catalog\GroupAccessTable::getList(array(
-						'select' => array('CATALOG_GROUP_ID'),
-						'filter' => array('GROUP_ID' => 2, '=ACCESS' => Catalog\GroupAccessTable::ACCESS_BUY),
-						'order' => array('CATALOG_GROUP_ID' => 'ASC')
-					));
-					while ($priceType = $priceIterator->fetch())
-					{
-						$priceTypeId = (int)$priceType['CATALOG_GROUP_ID'];
-						$priceTypes[$priceTypeId] = $priceTypeId;
-						unset($priceTypeId);
-					}
-					Catalog\Discount\DiscountManager::preloadPriceData(array($priceProductId), $priceTypes);
-					Catalog\Product\Price::loadRoundRules($priceTypes);
-				}
-			}
-			$optimalPrice = \CCatalogProduct::GetOptimalPrice($priceProductId, 1, array(2), 'N', array(), $this->getSiteId(), array());
-			$counterData['price'] = $optimalPrice['RESULT_PRICE']['DISCOUNT_PRICE'];
-			$counterData['currency'] = $optimalPrice['RESULT_PRICE']['CURRENCY'];
-
-			// make sure it is in utf8
-			$counterData = Main\Text\Encoding::convertEncoding($counterData, SITE_CHARSET, 'UTF-8');
-
-			// pack value and protocol version
-			$rcmLogCookieName = Main\Config\Option::get('main', 'cookie_name', 'BITRIX_SM') . '_' . Main\Analytics\Catalog::getCookieLogName();
-
-			$this->arResult['counterData'] = array(
-				'item' => base64_encode(json_encode($counterData)),
-				'user_id' => new Main\Text\JsExpression(
-					'function(){return BX.message("USER_ID") ? BX.message("USER_ID") : 0;}'
-				),
-				'recommendation' => new Main\Text\JsExpression(
-					'function() {
-							var rcmId = "";
-							var cookieValue = BX.getCookie("' . $rcmLogCookieName . '");
-							var productId = ' . $element["ID"] . ';
-							var cItems = [];
-							var cItem;
-
-							if (cookieValue)
-							{
-								cItems = cookieValue.split(".");
-							}
-
-							var i = cItems.length;
-							while (i--)
-							{
-								cItem = cItems[i].split("-");
-								if (cItem[0] == productId)
-								{
-									rcmId = cItem[1];
-									break;
-								}
-							}
-
-							return rcmId;
-						}'
-				),
-				'v' => '2'
-			);
-			$resultCacheKeys[] = 'counterData';
-
-			if ($this->arParams['SET_VIEWED_IN_COMPONENT'] === 'Y')
-			{
-				$viewedProduct = array(
-					'PRODUCT_ID' => $element['ID'],
-					'OFFER_ID' => $element['ID']
-				);
-
-				if (!empty($element['OFFERS']))
-				{
-					$viewedProduct['OFFER_ID'] = $element['OFFER_ID_SELECTED'] > 0
-						? $element['OFFER_ID_SELECTED']
-						: $element['OFFERS'][0]['ID'];
-				}
-
-				$this->arResult['VIEWED_PRODUCT'] = $viewedProduct;
-				$resultCacheKeys[] = 'VIEWED_PRODUCT';
-				unset($viewedProduct);
-			}
-			unset($element);
-		}
+		return $arParams;
 	}
 }
