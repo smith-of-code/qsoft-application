4cebfbe3d29f473670d2b4eca5f6230054af7b43
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-23 15:43:06 +0300

    000000 / Фикс enum полей legal_entity

--- app/core/local/src/ORM/LegalEntityTable.php
+++ app/core/local/src/ORM/LegalEntityTable.php
@@ -2,6 +2,8 @@
 
 namespace QSoft\ORM;
 
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\Entity;
 
@@ -22,6 +24,26 @@ class LegalEntityTable extends Entity\DataManager
 
     public static function getMap(): array
     {
+        if (!Loader::includeModule('highloadblock')) {
+            throw new \RuntimeException('Module highloadblock not found');
+        }
+
+        $hlBlock = HighloadBlockTable::getRow(['filter' => ['=TABLE_NAME' => self::getTableName()]]);
+        if (!$hlBlock) {
+            throw new \RuntimeException(sprintf('Не найден hl-блок %s', self::getTableName()));
+        }
+
+        $fields = \CUserTypeEntity::GetList([], ['ENTITY_ID' => 'HLBLOCK_' . $hlBlock['ID']]);
+
+        $statusIds = [];
+        while ($field = $fields->Fetch()) {
+            if ($field['FIELD_NAME'] === 'UF_STATUS') {
+                $enums = \CUserFieldEnum::GetList([], ['USER_FIELD_ID' => $field['ID']])->arResult;
+
+                $statusIds = array_column($enums, 'ID');
+            }
+        }
+
         return [
             new Entity\IntegerField('ID', [
                 'primary' => true,
@@ -34,7 +56,7 @@ class LegalEntityTable extends Entity\DataManager
             ]),
             new Entity\EnumField('UF_STATUS', [
                 'required' => true,
-                'values' => self::STATUSES,
+                'values' => $statusIds,
                 'title' => Loc::getMessage('LEGAL_ENTITY_UF_STATUS_FIELD'),
             ]),
         ];
