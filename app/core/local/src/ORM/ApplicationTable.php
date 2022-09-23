e5c9d0319541e0e07f8d615309880f90dfe1a8b3
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-23 15:40:18 +0300

    000000 / Фикс enum полей application

--- app/core/local/src/ORM/ApplicationTable.php
+++ app/core/local/src/ORM/ApplicationTable.php
@@ -2,6 +2,8 @@
 
 namespace QSoft\ORM;
 
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\Entity;
 
@@ -9,28 +11,43 @@ Loc::loadMessages(__FILE__);
 
 class ApplicationTable extends Entity\DataManager
 {
-    const TYPES = [
-        'APPLICATION_TYPE_CHANGE_PERSONAL_DATA',
-        'APPLICATION_TYPE_CHANGE_MENTOR',
-        'APPLICATION_TYPE_BUG',
-        'APPLICATION_TYPE_RETURN_ORDER',
-        'APPLICATION_TYPE_OTHER',
-    ];
-
-    const STATUSES = [
-        'APPLICATION_STATUS_NEW',
-        'APPLICATION_STATUS_IN_PROGRESS',
-        'APPLICATION_STATUS_DONE',
-        'APPLICATION_STATUS_REJECTED',
-    ];
-
     public static function getTableName(): string
     {
         return 'application';
     }
 
+    /**
+     * @return array
+     * @throws \Bitrix\Main\LoaderException
+     * @throws \Bitrix\Main\SystemException
+     */
     public static function getMap(): array
     {
+        if (!Loader::includeModule('highloadblock')) {
+            throw new \RuntimeException('Module highloadblock not found');
+        }
+
+        $hlBlock = HighloadBlockTable::getRow(['filter' => ['=TABLE_NAME' => self::getTableName()]]);
+        if (!$hlBlock) {
+            throw new \RuntimeException(sprintf('Не найден hl-блок %s', self::getTableName()));
+        }
+
+        $fields = \CUserTypeEntity::GetList([], ['ENTITY_ID' => 'HLBLOCK_' . $hlBlock['ID']]);
+
+        $typeIds = [];
+        $statusIds = [];
+        while ($field = $fields->Fetch()) {
+            if ($field['FIELD_NAME'] === 'UF_TYPE') {
+                $enums = \CUserFieldEnum::GetList([], ['USER_FIELD_ID' => $field['ID']])->arResult;
+
+                $typeIds = array_column($enums, 'ID');
+            } else if ($field['FIELD_NAME'] === 'UF_STATUS') {
+                $enums = \CUserFieldEnum::GetList([], ['USER_FIELD_ID' => $field['ID']])->arResult;
+
+                $statusIds = array_column($enums, 'ID');
+            }
+        }
+
         return [
             new Entity\IntegerField('ID', [
                 'primary' => true,
@@ -43,12 +60,12 @@ class ApplicationTable extends Entity\DataManager
             ]),
             new Entity\EnumField('UF_TYPE', [
                 'required' => true,
-                'values' => self::TYPES,
+                'values' => $typeIds,
                 'title' => Loc::getMessage('APPLICATION_ENTITY_UF_TYPE_FIELD'),
             ]),
             new Entity\EnumField('UF_STATUS', [
                 'required' => true,
-                'values' => self::STATUSES,
+                'values' => $statusIds,
                 'title' => Loc::getMessage('APPLICATION_ENTITY_UF_STATUS_FIELD'),
             ]),
             new Entity\StringField('UF_COMMENT', [
