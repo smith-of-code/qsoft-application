14c64cd372ad709c47f4e8e0c1df688a5b240fff
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-23 12:02:48 +0300

    226086 / Удалил неиспользуемые файлы

--- app/core/local/components/zolo/catalog.element/templates/.default/lang/ru/.parameters.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$MESS['PARAMETER_IBLOCK_TYPE_NAME'] = 'Тип инфоблока';
-$MESS['PARAMETER_IBLOCK_ID_NAME'] = 'Инфоблок';
-$MESS['PARAMETER_ELEMENT_ID_NAME'] = 'ID элемента';
-$MESS['PARAMETER_ELEMENT_CODE_NAME'] = 'Символьный код элемента';
-$MESS['PARAMETER_SECTION_ID_NAME'] = 'ID раздела';
-$MESS['PARAMETER_SECTION_CODE_NAME'] = 'Символьный код раздела';
