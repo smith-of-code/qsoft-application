e3d33c37c2e8e35ce044af2ad61fd10ad5abd796
Nikirama <n.ramenskiy@qsoft.ru>
2022-09-26 13:51:08 +0300

    225916 / Change php_interface stucture

--- app/core/local/php_interface/init.php
+++ app/core/local/php_interface/init.php
@@ -1,8 +1,3 @@
 <?php
 
-use QSoft\Events\User\UserEventsListener;
-
-/**
- * Main module events
- */
-AddEventHandler('main', 'OnBeforeUserUpdate', [UserEventsListener::class, 'OnBeforeUserUpdate']);
+require_once __DIR__ . '/events.php';
