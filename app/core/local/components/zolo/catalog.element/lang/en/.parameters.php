9bd1a3be3d44aee962efa97c23b4fb27f4deb281
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-28 19:56:34 +0300

    226651 / lang

--- app/core/local/components/zolo/catalog.element/lang/en/.parameters.php
+++ app/core/local/components/zolo/catalog.element/lang/en/.parameters.php
@@ -1,1 +1,14 @@
 <?php
+
+$MESS['PARAMETER_IBLOCK_TYPE_NAME'] = 'IBlock type';
+$MESS['PARAMETER_IBLOCK_ID_NAME'] = 'IBlock ID';
+$MESS['PARAMETER_ELEMENT_ID_NAME'] = 'Element ID';
+$MESS['PARAMETER_ELEMENT_CODE_NAME'] = 'Element code';
+$MESS['PARAMETER_SECTION_ID_NAME'] = 'Section ID';
+$MESS['PARAMETER_SECTION_CODE_NAME'] = 'Section code';
+$MESS['PARAMETER_SET_BROWSER_TITLE'] = 'Set browser title';
+$MESS['PARAMETER_SET_META_KEYWORDS'] = 'Set meta keywords';
+$MESS['IBLOCK_FIELD_NAME'] = 'Name';
+$MESS['CP_BCE_SET_META_KEYWORDS'] = 'Set meta keywords';
+$MESS['T_IBLOCK_DESC_KEYWORDS'] = 'Set page keywords from property';
+$MESS['T_IBLOCK_DESC_DESCRIPTION'] = 'Set page description from property';
--- app/core/local/components/zolo/catalog.element/lang/ru/.parameters.php
+++ app/core/local/components/zolo/catalog.element/lang/ru/.parameters.php
@@ -1,1 +1,14 @@
 <?php
+
+$MESS['PARAMETER_IBLOCK_TYPE_NAME'] = 'Тип инфоблока';
+$MESS['PARAMETER_IBLOCK_ID_NAME'] = 'ID инфоблока';
+$MESS['PARAMETER_ELEMENT_ID_NAME'] = 'ID элемента';
+$MESS['PARAMETER_ELEMENT_CODE_NAME'] = 'Символьный код элемента';
+$MESS['PARAMETER_SECTION_ID_NAME'] = 'ID раздела';
+$MESS['PARAMETER_SECTION_CODE_NAME'] = 'Символьный код раздела';
+$MESS['PARAMETER_SET_BROWSER_TITLE'] = 'Устанавливать заголовок окна браузера';
+$MESS['PARAMETER_SET_META_KEYWORDS'] = 'Устанавливать ключевые слова страницы';
+$MESS['IBLOCK_FIELD_NAME'] = 'Название';
+$MESS['CP_BCE_SET_META_KEYWORDS'] = 'Устанавливать ключевые слова страницы';
+$MESS['T_IBLOCK_DESC_KEYWORDS'] = 'Установить ключевые слова страницы из свойства';
+$MESS['T_IBLOCK_DESC_DESCRIPTION'] = 'Установить описание страницы из свойства';
