2f5e079261f034e6f87b335f17c0f0fb0f241b8d
Мотин Михаил Сергеевич <m.motin@qsoft.ru>
2022-09-28 19:52:20 +0300

    226651 / lang

---
+++ app/core/local/components/zolo/catalog.related.products/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS ['T_IBLOCK_DESC_CATALOG'] = 'Каталог';
+$MESS ['IBLOCK_ELEMENT_TEMPLATE_NAME'] = 'Сопутствующие товары';
+$MESS ['IBLOCK_ELEMENT_TEMPLATE_DESCRIPTION'] = 'Выводит список сопутствующих товаров';
