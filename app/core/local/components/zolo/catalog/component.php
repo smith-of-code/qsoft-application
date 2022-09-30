82783b41093e426ce5f2e8abf655b4bbe6ea5cc3
Nikirama <n.ramenskiy@qsoft.ru>
2022-09-30 15:02:26 +0300

    Fix categories

--- app/core/local/components/zolo/catalog/component.php
+++ app/core/local/components/zolo/catalog/component.php
@@ -54,7 +54,6 @@ if($arParams["SEF_MODE"] == "Y")
 		$arVariables
 	);
 
-    // ������ ��������� ��� �������� /catalog/ - ��������� �������� ������
 	if (! isset($arVariables['SECTION_CODE'])) {
         $arVariables['SECTION_CODE'] = '';
         $componentPage = "section";
@@ -103,8 +102,23 @@ if($arParams["SEF_MODE"] == "Y")
 
 	CComponentEngine::initComponentVariables($componentPage, $arComponentVariables, $arVariableAliases, $arVariables);
 
-    if (empty($arVariables["SMART_FILTER_PATH"]))
-        $arVariables["SMART_FILTER_PATH"] = explode("?", $_SERVER["REQUEST_URI"])[0];
+    $arVariables["SMART_FILTER_PATH"] = explode("?", $_SERVER["REQUEST_URI"])[0];
+
+    if(!empty($arVariables["SECTION_ID"]))
+    {
+        if (!empty($arVariables["SMART_FILTER_PATH"]))
+        {
+            $smartParts = array_reverse(explode("/", $arVariables["SMART_FILTER_PATH"]));
+            foreach($smartParts as $smartPart)
+            {
+                if(!empty($smartPart) && $arSection = CIBlockSection::GetList([], ['CODE' => $smartPart])->GetNext())
+                {
+                    $arVariables["SECTION_ID"] = $arSection["ID"];
+                    break;
+                }
+            }
+        }
+    }
 
 	$arResult = array(
 		"FOLDER" => $arParams["SEF_FOLDER"],
