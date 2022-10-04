f5348ad94cd31f0bede05e1d60508a298d7b28a8
Andrey Lebedev <a.lebedev@qsoft.ru>
2022-10-04 13:45:55 +0300

    226655 / Небольшие правки миграций

--- app/core/local/src/Migrate/2022_10_04_105940_AddUserGroupsForLoyaltyProgram.php
+++ app/core/local/src/Migrate/2022_10_04_105940_AddUserGroupsForLoyaltyProgram.php
@@ -10,19 +10,19 @@ class AddUserGroupsForLoyaltyProgram extends Migration
     private array $groups = [
         [
             'NAME' => 'Программа лояльности - K1',
-            'STRING_ID' => 'loyalty_k1',
+            'STRING_ID' => 'loyalty_K1',
             'ACTIVE' => 'Y',
             'C_SORT' => 400,
         ],
         [
             'NAME' => 'Программа лояльности - K2',
-            'STRING_ID' => 'loyalty_k2',
+            'STRING_ID' => 'loyalty_K2',
             'ACTIVE' => 'Y',
             'C_SORT' => 400,
         ],
         [
             'NAME' => 'Программа лояльности - K3',
-            'STRING_ID' => 'loyalty_k3',
+            'STRING_ID' => 'loyalty_K3',
             'ACTIVE' => 'Y',
             'C_SORT' => 400,
         ],
