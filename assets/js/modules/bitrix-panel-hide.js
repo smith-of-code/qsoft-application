00c01d802d2f59f4e6e5d9899ae0505d00ca15e3
Nikolay Mamaenko <n.mamaenko@qsoft.ru>
2022-10-14 19:22:36 +0300

    Скрипт добавления класса на панель битрикса

--- assets/js/modules/bitrix-panel-hide.js
+++ assets/js/modules/bitrix-panel-hide.js
@@ -2,10 +2,15 @@ const ID = {
     bitrix: '#bx-panel',
 };
 
+const CLASS = {
+    fixed: 'bx-panel-fixed',
+};
+
 export default function () {
     const bitrixPanel = document.querySelector(ID.bitrix);
 
     if (bitrixPanel) return;
 
     document.querySelector('body').style.paddingTop = `${bitrixPanel.offsetHeight}px`;
+    bitrixPanel.classList.add(CLASS.fixed);
 };
