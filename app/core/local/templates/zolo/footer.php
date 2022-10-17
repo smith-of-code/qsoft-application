af7aa1ea3bbcf1241d1bfd183e247255ee9c56e9
Ксения Петренко <k.petrenko@zolo-vpool>
2022-10-17 19:17:01 +0300

    Revert "Временная замена хедера и футера на верстку"
    This reverts commit a3296b9436dcc46e511c914293fe739719e51d6c.

--- app/core/local/templates/zolo/footer.php
+++ app/core/local/templates/zolo/footer.php
@@ -36,204 +36,188 @@
 		</div><!--end .container.bx-content-section-->
 	</div><!--end .workarea-->
 
-<!--Футер-->
-<footer class="page__footer footer">
-    <div class="footer__container container">
-        <nav class="footer__nav">
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Для собак
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Сухой корм
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Влажный корм
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Лакомства
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Аксессуары
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Советы экспертов
-                    </a>
-                </li>
-            </ul>
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Для кошек
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Сухой корм
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Влажный корм
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Лакомства
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Аксессуары
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Советы экспертов
-                    </a>
-                </li>
-            </ul>
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Покупателям
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Акции
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Оплата и доставка
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        FAQ
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Обратиться в поддержку
-                    </a>
-                </li>
-            </ul>
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Компания AmeAppetite
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        AmeБизнес
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Новости
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Мероприятия
-                    </a>
-                </li>
-            </ul>
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Контакты
-                </li>
-                <li class="footer__item">
-                    <a href="tel:88001234567" class="footer__link">
-                        8 (800) 123-45-67
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="mailto:dogcathappy@ame.ru" class="footer__link">
-                        dogcathappy@ame.ru
-                    </a>
-                </li>
-                <li class="footer__item">
-                    Москва, Проспект Мира, 87
-                </li>
-            </ul>
-            <ul class="footer__list">
-                <li class="footer__item footer__item--heading">
-                    Правовая информация
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Правила компании
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Положение об обработке персональных данных
-                    </a>
-                </li>
-                <li class="footer__item">
-                    <a href="#" class="footer__link">
-                        Правила поведения в компании
-                    </a>
-                </li>
-            </ul>
-        </nav>
-
-        <div class="footer__social social">
-            <ul class="social__list">
-                <li class="social__item">
-                    <a href="#" class="social__link">
-                        <svg class="icon icon--social">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-telegram"></use>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <a href="#" class="social__link">
-                        <svg class="icon icon--social">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-youtube"></use>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <a href="#" class="social__link">
-                        <svg class="icon icon--social">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-vk"></use>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <a href="#" class="social__link">
-                        <svg class="icon icon--social">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-whatsapp"></use>
-                        </svg>
-                    </a>
-                </li>
-                <li class="social__item">
-                    <a href="#" class="social__link">
-                        <svg class="icon icon--social">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-viber"></use>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="footer__bottom">
-            <p class="footer__copyright">
-                &copy; AmeAppetite, 2022
-            </p>
-        </div>
-    </div>
-</footer>
-<!--/Футер-->
-
+	<footer class="bx-footer">
+		<div class="bx-footer-section bx-footer-bg">
+			<div class="container">
+				<?$APPLICATION->IncludeComponent(
+					"bitrix:main.include",
+					"",
+					Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_DIR."include/socnet_footer.php",
+						"AREA_FILE_RECURSIVE" => "N",
+						"EDIT_MODE" => "html",
+					),
+					false,
+					Array('HIDE_ICONS' => 'Y')
+				);?>
+			</div>
+		</div>
+		<div class="bx-footer-section py-5 bg-dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-lg-3 order-lg-2 order-1 mb-4 mb-lg-0">
+						<h4 class="bx-block-title text-light">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => SITE_DIR."include/about_title.php"
+								),
+								false
+							);?>
+						</h4>
+						<? $APPLICATION->IncludeComponent(
+							"bitrix:menu",
+							"bottom_menu",
+							array(
+								"ROOT_MENU_TYPE" => "bottom",
+								"MAX_LEVEL" => "1",
+								"MENU_CACHE_TYPE" => "A",
+								"CACHE_SELECTED_ITEMS" => "N",
+								"MENU_CACHE_TIME" => "36000000",
+								"MENU_CACHE_USE_GROUPS" => "Y",
+								"MENU_CACHE_GET_VARS" => array(),
+							),
+							false
+						);?>
+					</div>
+					<div class="col-sm-6 col-lg-3 order-lg-3 order-2 mb-4 mb-lg-0">
+						<h4 class="bx-block-title text-light">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => SITE_DIR."include/catalog_title.php"
+								),
+								false
+							);?>
+						</h4>
+						<?$APPLICATION->IncludeComponent(
+							"bitrix:menu",
+							"bottom_menu",
+							array(
+								"ROOT_MENU_TYPE" => "left",
+								"MENU_CACHE_TYPE" => "A",
+								"MENU_CACHE_TIME" => "36000000",
+								"MENU_CACHE_USE_GROUPS" => "Y",
+								"MENU_CACHE_GET_VARS" => array(),
+								"CACHE_SELECTED_ITEMS" => "N",
+								"MAX_LEVEL" => "1",
+								"USE_EXT" => "Y",
+								"DELAY" => "N",
+								"ALLOW_MULTI_SELECT" => "N"
+							),
+							false
+						);?>
+					</div>
+					<div class="col-sm-6 col-lg-3 order-lg-4 order-3">
+						<div style="padding: 20px;background:#eaeaeb">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => SITE_DIR."include/sender.php",
+									"AREA_FILE_RECURSIVE" => "N",
+									"EDIT_MODE" => "html",
+								),
+								false,
+								array('HIDE_ICONS' => 'Y')
+							);?>
+						</div>
+						<div id="bx-composite-banner" style="padding-top: 20px"></div>
+					</div>
+					<div class="col-sm-6 col-lg-3 order-lg-1 order-4">
+						<div class="mb-3">
+							<a class="bx-footer-logo" href="<?=SITE_DIR?>">
+								<? $APPLICATION->IncludeComponent(
+									"bitrix:main.include",
+									"",
+									array(
+										"AREA_FILE_SHOW" => "file",
+										"PATH" => SITE_DIR."include/company_logo_mobile.php"
+									),
+								false
+								);?>
+							</a>
+						</div>
+						<div class="mb-3 d-flex align-items-center">
+							<i class="fa fa-phone pr-3 text-white" style="font-size: 25px;"></i>
+							<span class="text-white">
+								<? $APPLICATION->IncludeComponent(
+									"bitrix:main.include",
+									"", array(
+										"AREA_FILE_SHOW" => "file",
+										"PATH" => SITE_DIR."include/telephone.php"
+									),
+									false
+								);?>
+							</span>
+						</div>
+						<div class="mb-3 text-white">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => SITE_DIR."include/schedule.php"
+								),
+								false
+							);?>
+						</div>
+						<div class="mb-3 text-white">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:main.include",
+								"",
+								array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => SITE_DIR."include/personal.php"
+								),
+								false
+							);?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="bx-footer-section py-2 bg-secondary">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 bx-up">
+							<a href="javascript:void(0)" data-role="eshopUpButton" class="text-white"><i class="fa fa-caret-up"></i> <?=GetMessage("FOOTER_UP_BUTTON")?></a>
+						</div>
+						<div class="col-sm-6 text-white text-right">
+							<? $APPLICATION->IncludeComponent("bitrix:main.include", "", array(
+								"AREA_FILE_SHOW" => "file",
+								"PATH" => SITE_DIR."include/copyright.php"
+							), false);?>
+						</div>
+					</div>
+				</div>
+			</div>
+	</footer>
+	<div class="col d-sm-none">
+		<?$APPLICATION->IncludeComponent("bitrix:sale.basket.basket.line", "bootstrap_v4", array(
+				"PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+				"PATH_TO_PERSONAL" => SITE_DIR."personal/",
+				"SHOW_PERSONAL_LINK" => "N",
+				"SHOW_NUM_PRODUCTS" => "Y",
+				"SHOW_TOTAL_PRICE" => "Y",
+				"SHOW_PRODUCTS" => "N",
+				"POSITION_FIXED" =>"Y",
+				"POSITION_HORIZONTAL" => "center",
+				"POSITION_VERTICAL" => "bottom",
+				"SHOW_AUTHOR" => "Y",
+				"PATH_TO_REGISTER" => SITE_DIR."login/",
+				"PATH_TO_PROFILE" => SITE_DIR."personal/"
+			),
+			false,
+			array()
+		);?>
+	</div>
 </div> <!-- //bx-wrapper -->
 
 
