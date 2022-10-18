17075800e0ff17034bde8cf25fdb99ef6965de08
Дмитрий Михайлов <d.mikhaylov@qsoft.ru>
2022-10-19 02:34:19 +0300

    Компонент мини-корзины для шапки

--- app/core/local/components/zolo/sale.basket.basket.line/templates/.default/template.php
+++ app/core/local/components/zolo/sale.basket.basket.line/templates/.default/template.php
@@ -1,43 +1,44 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /**
  * @global string $componentPath
  * @global string $templateName
  * @var CBitrixComponentTemplate $this
  */
 $cartStyle = 'bx-basket';
-$cartId = "bx_basket".$this->randString();
+$cartId = "bx_basket" . $this->randString();
 $arParams['cartId'] = $cartId;
 
-if ($arParams['POSITION_FIXED'] == 'Y')
-{
-	$cartStyle .= "-fixed {$arParams['POSITION_HORIZONTAL']} {$arParams['POSITION_VERTICAL']}";
-	if ($arParams['SHOW_PRODUCTS'] == 'Y')
-		$cartStyle .= ' bx-closed';
-}
-else
-{
-	$cartStyle .= ' bx-opener';
-}
-?><script>
-var <?=$cartId?> = new BitrixSmallCart;
+//if ($arParams['POSITION_FIXED'] == 'Y')
+//{
+//	$cartStyle .= "-fixed {$arParams['POSITION_HORIZONTAL']} {$arParams['POSITION_VERTICAL']}";
+//	if ($arParams['SHOW_PRODUCTS'] == 'Y')
+//		$cartStyle .= ' bx-closed';
+//}
+//else
+//{
+//	$cartStyle .= ' bx-opener';
+//}
+
+
+?>
+<div class="personal__item" id="<?= $cartId ?>">
+    <button type="button" class="button button--simple button--red button--vertical">
+        <span class="button__icon button__icon--mixed">
+            <svg class="icon icon--basket">
+                <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-basket"></use>
+            </svg>
+
+            <span class="button__icon-counter button__icon-counter--dark">12</span>
+        </span>
+        <span class="personal__button-text button__text">16 842 ₽</span>
+    </button>
+</div>
+
+<script>
+    let <?= $cartId ?> = new BitrixSmallCart;
 </script>
-<div id="<?=$cartId?>" class="<?=$cartStyle?>"><?
-	/** @var \Bitrix\Main\Page\FrameBuffered $frame */
-	$frame = $this->createFrame($cartId, false)->begin();
-		require(realpath(dirname(__FILE__)).'/ajax_template.php');
-	$frame->beginStub();
-		$arResult['COMPOSITE_STUB'] = 'Y';
-		require(realpath(dirname(__FILE__)).'/top_template.php');
-		unset($arResult['COMPOSITE_STUB']);
-	$frame->end();
-?></div>
 <script type="text/javascript">
-	<?=$cartId?>.siteId       = '<?=SITE_ID?>';
-	<?=$cartId?>.cartId       = '<?=$cartId?>';
-	<?=$cartId?>.ajaxPath     = '<?=$componentPath?>/ajax.php';
-	<?=$cartId?>.templateName = '<?=$templateName?>';
-	<?=$cartId?>.arParams     =  <?=CUtil::PhpToJSObject ($arParams)?>; // TODO \Bitrix\Main\Web\Json::encode
-	<?=$cartId?>.closeMessage = '<?=GetMessage('TSB1_COLLAPSE')?>';
-	<?=$cartId?>.openMessage  = '<?=GetMessage('TSB1_EXPAND')?>';
-	<?=$cartId?>.activate();
+    <?=$cartId?>.siteId = '<?=SITE_ID?>';
+    <?=$cartId?>.cartId = '<?=$cartId?>';
+    <?=$cartId?>.activate();
 </script>
