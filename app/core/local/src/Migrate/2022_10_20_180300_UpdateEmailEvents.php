5472fc4cf7bd5fc71f4442dd9c3a1c9eaf53abb9
Nikirama <n.ramenskiy@qsoft.ru>
2022-10-21 11:37:27 +0300

    Вырезал лишнее

--- app/core/local/src/Migrate/2022_10_20_180300_UpdateEmailEvents.php
+++ app/core/local/src/Migrate/2022_10_20_180300_UpdateEmailEvents.php
@@ -47,44 +47,5 @@ Login: #LOGIN#
 Сообщение сгенерировано автоматически.',
             ],
         ],
-        [
-            'event_name' => 'USER_PASS_REQUEST',
-            'from' => [
-                'MESSAGE' => 'Информационное сообщение сайта #SITE_NAME#
-------------------------------------------
-#NAME# #LAST_NAME#,
-
-#MESSAGE#
-
-Для смены пароля перейдите по следующей ссылке:
-http://#SERVER_NAME#/auth/index.php?change_password=yes&lang=ru&USER_CHECKWORD=#CHECKWORD#&USER_LOGIN=#URL_LOGIN#
-
-Ваша регистрационная информация:
-
-ID пользователя: #USER_ID#
-Статус профиля: #STATUS#
-Login: #LOGIN#
-
-Сообщение сгенерировано автоматически.',
-            ],
-            'to' => [
-                'MESSAGE' => 'Информационное сообщение сайта #SITE_NAME#
-------------------------------------------
-#NAME# #LAST_NAME#,
-
-#MESSAGE#
-
-Для смены пароля перейдите по следующей ссылке:
-http://#SERVER_NAME#/login?change_password=yes&lang=ru&user_id=#USER_ID#&confirm_code=#CONFIRM_CODE#
-
-Ваша регистрационная информация:
-
-ID пользователя: #USER_ID#
-Статус профиля: #STATUS#
-Login: #LOGIN#
-
-Сообщение сгенерировано автоматически.',
-            ],
-        ],
     ];
 }
