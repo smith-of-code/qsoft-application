c3e176de6049db02e818730a23ad2182a8faed6b
Andrey Lebedev <a.lebedev@qsoft.ru>
2022-10-24 17:58:02 +0300

    227012 / Интегрирована основная структура

--- app/core/local/components/zolo/catalog/templates/.default/section_vertical.php
+++ app/core/local/components/zolo/catalog/templates/.default/section_vertical.php
@@ -35,37 +35,6 @@ else
 
 <div class="content__main">
     <div class="catalog__wrapper">
-
-        <?
-        /* Список разделов каталога */
-        $sectionListParams = array(
-            "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-            "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-            "SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
-            "SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-            "CACHE_TYPE" => $arParams["CACHE_TYPE"],
-            "CACHE_TIME" => $arParams["CACHE_TIME"],
-            "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-            "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
-            "TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
-            "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-            "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
-            "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-            "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
-            "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
-        );
-        $sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_ACTIVE";
-        $sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_AVAILABLE";
-        $APPLICATION->IncludeComponent(
-            "zolo:catalog.section.list",
-            "",
-            $sectionListParams,
-            $component,
-            array("HIDE_ICONS" => "Y")
-        );
-        unset($sectionListParams);
-        ?>
-
         <?
         /* Фильтр */
         $APPLICATION->IncludeComponent("zolo:catalog.smart.filter", "", array(
@@ -95,7 +64,6 @@ else
             array('HIDE_ICONS' => 'Y')
         );
         ?>
-
 
         <div class="catalog__main">
             <div class="catalog__panel">
