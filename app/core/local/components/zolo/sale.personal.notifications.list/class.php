8f960c11d80d978f37d76f0e5d0beed9b0b5ff96
Федор Ревякин <f.revakin@qsoft.ru>
2022-10-25 13:52:19 +0300

    227016 / Добавил фильтрацию по дням и статусам

--- app/core/local/components/zolo/sale.personal.notifications.list/class.php
+++ app/core/local/components/zolo/sale.personal.notifications.list/class.php
@@ -1,45 +1,91 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true);
 
 use QSoft\Entity\User;
+use \Bitrix\Main\ORM\Query\Filter\ConditionTree;
+use \Bitrix\Main\ORM\Query\Query;
+use \Bitrix\Main\Engine\Contract\Controllerable;
+use \Bitrix\Main\Engine\ActionFilter;
 ?>
 
 <?php
 
-class NotificationListComponent extends CBitrixComponent implements \Bitrix\Main\Engine\Contract\Controllerable
+class NotificationListComponent extends CBitrixComponent implements Controllerable
 {
+    private const NOTIFICATIONS_LIMIT = 2;
+
     public function configureActions()
     {
-        return [];
+        return [
+            'loadNotifications' => [
+                '-prefilters' => [
+                    ActionFilter\Csrf::class,
+                    ]
+                ]
+            ];
     }
-    private const NOTIFICATIONS_LIMIT = 1;
+
     public function executeComponent()
     {
         $this->arResult = $this->loadNotificationsAction();
         $this->includeComponentTemplate();
     }
 
-    private function loadNotificationsAction($offset = 0): array
+    public function loadNotificationsAction(array $parameters = []): array
     {
-        $user = new User();
-        $notificationTable = $user->notification->getDataClass();
+        self::prepareParameters($parameters);
+        return self::loadNotificationsData($parameters, self::createFilter($parameters));
+    }
+
+    private static function loadNotificationsData(array $parameters, ConditionTree $filter): array
+    {
+        $notificationTable = (new User())->notification->getDataClass();
         $notifications = $notificationTable::getList([
                 'select' => [
-                    'NAME'=> 'TYPE_NAME.VALUE',
+                    'TITLE' => 'UF_TITLE',
                     'STATUS' => 'STATUS_NAME.VALUE',
                     'TEXT' => 'UF_MESSAGE',
                     'LINK' => 'UF_LINK',
-                    'DATE' => 'UF_DATE_TIME',
+                    'DATE',
+                    'TIME',
                 ],
-                'filter' => ['UF_USER_ID' => $user->id],
-                'offset' => $offset,
-                'limit' => self::NOTIFICATIONS_LIMIT,
+                'filter' => $filter,
+                'offset' => $parameters['offset'],
+                'limit' => $parameters['limit'],
+                'order' => ["UF_DATE_TIME" => "desc"]
             ]
         )->fetchAll();
         return [
             'NOTIFICATIONS' => $notifications,
-            'OFFSET' => $offset + count($notifications),
+            'OFFSET' => $parameters['offset'] + count($notifications),
         ];
     }
 
+    private static function prepareParameters(array &$parameters): void
+    {
+        if(empty($parameters)) {
+            $parameters['offset'] = 0;
+            $parameters['limit'] = self::NOTIFICATIONS_LIMIT;
+            return;
+        }
+        if(!$parameters['offset']) {
+            $parameters['limit'] = $parameters['size'];
+        } else {
+            $parameters['limit'] = self::NOTIFICATIONS_LIMIT;
+        }
+    }
 
+    private static function createFilter(array $filterParameters): ConditionTree
+    {
+        $filter = Query::filter();
+        if($filterParameters['filter']['day_interval']) {
+            $filter->where('DATE_DIFF', '<=', $filterParameters['filter']['day_interval']);
+        }
+        if($filterParameters['filter']['status']) {
+            $filter->where('STATUS', $filterParameters['filter']['status']);
+        }
+
+        $filter->where('UF_USER_ID', (new User())->id);
+
+        return $filter;
+    }
 }
--- app/core/local/components/zolo/sale.personal.notifications.list/templates/.default/template.php
+++ app/core/local/components/zolo/sale.personal.notifications.list/templates/.default/template.php
@@ -6,44 +6,78 @@
     <div>
         <? foreach ($arResult['NOTIFICATIONS'] as $notification) :?>
             Уведомление:</br>
-            Заголовок - <?="\"{$notification['NAME']}\""?><br>
-            Текст уведомления - <?="\"{$notification['TEXT']}\""?><br>
-            Дата создания уведомления - <?="\"{$notification['DATE']}\""?><br>
-            Время отправки уведомления - <?=null?><br>
+            Заголовок - <?=$notification['TITLE']?><br>
+            Текст уведомления - <?=$notification['TEXT']?><br>
+            Дата создания уведомления - <?=$notification['DATE']?><br>
+            Время отправки уведомления - <?=$notification['TIME']?><br>
             Статус уведомления - <?=$notification['STATUS']?><br>
             Ссылка - <?=$notification['LINK']?><br>
         <?endforeach;?>
     </div>
 </div>
 
-<button id="button" onclick="loadNotifications()">Показать еще</button>
+<button id="button" onclick="nextNotifications()">Показать еще</button>
+<button id="filterRead" onclick="notificationsFilterRead()">Фильтр: Прочитано</button>
+<button id="filterRead" onclick="notificationsFilterNotRead()">Фильтр: Не прочитано</button>
+<button id="filterRead" onclick="notificationsFilterReset()">Фильтр: Все уведомления</button>
+
 
 <script>
     let offset = <?=$arResult['OFFSET']?>;
+    let filter = {'day_interval': 30};
 
-    function loadNotifications() {
+    function nextNotifications() {
+        loadNotifications({'offset': offset, 'filter': filter});
+    }
+
+    function notificationsFilterRead() {
+        filter['status'] = 'прочитано';
+        loadNotifications({'size': offset, 'filter': filter});
+    }
+
+    function notificationsFilterNotRead() {
+        filter['status'] = 'не прочитано';
+        loadNotifications({'size': offset, 'filter': filter});
+    }
+
+    function notificationsFilterReset() {
+        filter['status'] = '';
+        loadNotifications({'size': offset, 'filter': filter});
+    }
+
+    function loadNotifications(parameters) {
         BX.ajax.runComponentAction('zolo:sale.personal.notifications.list', 'loadNotifications', {
             mode: 'class',
             data: {
-                offset: offset,
+                parameters: parameters
             }
         }).then(function (response) {
             console.log(response);
             offset = response['data']['OFFSET'];
-            attach(response['data']['NOTIFICATIONS']);
+            if('size' in parameters) {
+                replace(response['data']['NOTIFICATIONS'])
+            } else {
+                attach(response['data']['NOTIFICATIONS'])
+            }
         }, function (response) {
+            //console.log(response);
             alert("Ошибка при вызове метода компонента-контроллера")
         });
+    }
+
+    function replace(notifications) {
+        document.getElementById("notificationList").innerHTML = '';
+        attach(notifications);
     }
 
     function attach(notifications) {
         for (let i = 0; i < Object.keys(notifications).length; i++) {
             let div = document.createElement('div');
             div.innerHTML += "Уведомление:" + "<br>";
-            div.innerHTML += "Заголовок" + " - " + notifications[i]['NAME'] + "<br>";
+            div.innerHTML += "Заголовок" + " - " + notifications[i]['TITLE'] + "<br>";
             div.innerHTML += "Текст уведомления" + " - " + notifications[i]['TEXT'] + "<br>";
-            div.innerHTML += "Дата создания уведомления" + " - " + "null" + "<br>";
-            div.innerHTML += "Время отправки уведомления" + " - " + "null" + "<br>";
+            div.innerHTML += "Дата создания уведомления" + " - " + notifications[i]['DATE'] + "<br>";
+            div.innerHTML += "Время отправки уведомления" + " - " + notifications[i]['TIME'] + "<br>";
             div.innerHTML += "Статус уведомления" + " - " + notifications[i]['STATUS'] + "<br>";
             div.innerHTML += "Ссылка" + " - " + notifications[i]['LINK'] + "<br>";
             document.getElementById("notificationList").appendChild(div);
@@ -51,3 +85,9 @@
     }
 
 </script>
+
+<!--
+loadNotifications(offset, filter): [NOTIFICATIONS=>[], OFFSET=>] // получить следующую часть уведомлений (пагинация)
+loadNotifications(size, filter): [NOTIFICATIONS=>[], OFFSET=>]  // фильтрованные текущие уведомления
+Объект фильтра filter не должен быть изменен перед вызовом метода пагинации отфильтрованных ранее данных .
+-->
