20e045d4ba3054aecfec6f39bdc3fbb3452117df
gn.kopylov <gn.kopylov@qsoft.ru>
2022-10-25 16:53:04 +0300

    226992 / Реализация страницы О компании

--- app/site/_markup/about/about.php
+++ app/site/_markup/about/about.php
@@ -492,7 +492,32 @@
 
                     <div class="content__main">
                         <div class="about__wrapper">
-                          
+                            <section class="about__hero section">
+                                
+                                <div class="about__hero-box section__box box box--gray box--rounded-sm">
+                                    <div class="about__hero-images">
+                                        <img class="about__hero-images-picture" src="/local/templates/.default/images/about-hero.png" alt="hero-img">
+                                    </div>
+                                    <h3 class="about__hero-title section__title section__title--closer">
+                                        AmeAppetite <br>
+                                        Заботимся мы — счастливее вы!
+                                    </h3>
+                                    <div class="about__hero-description">
+                                        <p class="about__hero-description-text">
+                                            Идея создания компании родилась из нашей любви как к животным, так и к людям. Мы не хотели выбирать и решили воплотить в жизнь мечту приносить пользу тем, кого любим. Мы стремимся заботиться о здоровье наших пушистых друзей и выбираем только качественную продукцию, а также даем возможность каждому стать нашим партнером и предлагаем привлекательные возможности для ведения своего бизнеса, чтобы в жизни каждого было больше ярких эмоций и запоминающихся событий.
+                                        </p>
+                                        <p class="about__hero-description-text">
+                                            Мы любим то, что делаем и хотим расти и становиться лучше вместе с Вами.
+                                        </p>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section class="about__appreciate section">
+                                <h3 class="section__title section__title--closer">
+                                    Мы ценим
+                                </h3>
+                            </section>
                         </div>
                     </div>
                 </main>
