d2142ae99d0e6075ba4699038a34a905966f79da
gn.kopylov <gn.kopylov@qsoft.ru>
2022-10-31 11:15:39 +0300

    226989 / Реализована детальная страница для Новостей, Советов, Мероприятий

---
+++ app/site/_markup/detail/detail-events.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Детальная страница</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="/local/templates/.default/css/style.css" />
+    </head>
+
+    <body class="page">
+
+        <!--header-->
+        <? include_once("../include/header.php"); ?>
+        <!--/header-->
+
+        <!--content-->
+        <div class="page__content page__content--breadcrumbs content">
+            <div class="container">
+                <main class="page__detail-news detail-news">
+                    <div class="breadcrumbs">
+                        <ul class="breadcrumbs__list">
+                            <li class="breadcrumbs__item">
+                                <a href="#" class="breadcrumbs__link">Главная</a>
+                            </li>
+                            <li class="breadcrumbs__item">
+                                <a href="#" class="breadcrumbs__link">Детальная страница</a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="content__main content__main--separated detail-news__content">
+                        <section class="section events__section">
+                            <div class="detail-news__header section__header">
+                                <h2 class="detail-news__title section__title">
+                                    Запускаем обучающие курсы для новых консультантов
+                                </h2>
+                                <div class="detail-news__date">10.08.2022</div>
+                            </div>
+
+                            <div class="detail-news__image detail-news__image--main">
+                                <img class="detail-news__image-picture" src="https://via.placeholder.com/1200x800" alt="">
+                            </div>
+
+                            <h3 class="detail-news__subtitle">
+                                Зачем нужно обучение?
+                            </h3>
+
+                            <p class="detail-news__text">
+                                Зная весь ассортимент компании, вы с легкостью всегда сможете проконсультировать своего клиента и ответить на все его вопросы. Вам необходимо знать состав и особенности каждой косметической линейки, чтобы грамотно подбирать средства для всех возрастных категорий. «Эйвон» проводит обучающие курсы по нанесению макияжа и по окрашиванию волос. Став представителем компании вы бесплатно освоите новую профессию стилиста и визажиста, благодаря чему приобретете еще больше клиентов. Навыки в сфере красоты и ухода за собой всегда будут востребованы. Вы сможете не только хорошо зарабатывать, но и стать эталоном красоты для своих клиентов, друзей и близких. Пройдя обучение «Эйвон» онлайн, вы станете специалистом высшего класса!
+                                Зная весь ассортимент компании, вы с легкостью всегда сможете проконсультировать своего клиента и ответить на все его вопросы. Вам необходимо знать состав и особенности каждой косметической линейки, чтобы грамотно подбирать средства для всех возрастных категорий. «Эйвон» проводит обучающие курсы по нанесению макияжа и по окрашиванию волос. Став представителем компании вы бесплатно освоите новую профессию стилиста и визажиста, благодаря чему приобретете еще больше клиентов. Навыки в сфере красоты и ухода за собой всегда будут востребованы. Вы сможете не только хорошо зарабатывать, но и стать эталоном красоты для своих клиентов, друзей и близких. Пройдя обучение «Эйвон» онлайн, вы станете специалистом высшего класса!
+                            </p>
+
+                            <div class="detail-news__image ">
+                                <img class="detail-news__image-picture" src="https://via.placeholder.com/1200x800" alt="">
+                            </div>
+
+                            <h3 class="detail-news__subtitle">
+                                Преимущества работы в нашей компании
+                            </h3>
+
+                            <p class="detail-news__text">
+                                Зная весь ассортимент компании, вы с легкостью всегда сможете проконсультировать своего клиента и ответить на все его вопросы. Вам необходимо знать состав и особенности каждой косметической линейки, чтобы грамотно подбирать средства для всех возрастных категорий
+                            </p>
+
+                            <div class="detail-news__list">
+                                <h5 class="detail-news__list-title">Преимущества:</h5>
+
+                                <ol>
+                                    <li>Возможность быстро проявить себя</li>
+                                    <li>Более свободное отношение к графику работы, отсутствие строгих корпоративных правил</li>
+                                    <li>Причастность к конечному результату работы компании</li>
+                                    <li>Разнообразный опыт</li>
+                                </ol>
+                            </div>
+
+                            <div class="detail-news__image ">
+                                <img class="detail-news__image-picture" src="https://via.placeholder.com/1200x800" alt="">
+                            </div>
+
+                            <h3 class="detail-news__subtitle">
+                                Эффективное общение с Клиентом
+                            </h3>
+
+                            <p class="detail-news__text">
+                                Avon поддерживает Вас на каждом этапе сотрудничества, и в рамках нашего курса Вы узнаете, как проводить встречи с Клиентами так, чтобы каждая из них приносила Вам заказ.
+                            </p>
+
+                            <div class="detail-news__list">
+                                <h5 class="detail-news__list-title">
+                                    Наш курс содержит следующие обучающие блоки:
+                                </h5>
+
+                                <ul class="list">
+                                    <li class="list__item">Возможность быстро проявить себя</li>
+                                    <li class="list__item">
+                                        Более свободное отношение к графику работы, отсутствие строгих корпоративных правил
+                                    </li>
+                                    <li class="list__item">Причастность к конечному результату работы компании</li>
+                                    <li class="list__item">Разнообразный опыт</li>
+                                </ul>
+                            </div>
+                        </section>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <!--content-->
+
+        <!--Футер-->
+        <? include_once("../include/footer.php"); ?>
+        <!--/Футер-->
+
+        <script src="/local/templates/.default/js/script.js"></script>
+    </body>
+
+</html>
