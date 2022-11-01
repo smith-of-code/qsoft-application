bae14eeaf28b67f65ea5f14ef6ee9bb396b133ad
Manerova Olga <o.manerova@qsoft.ru>
2022-11-01 14:06:34 +0300

    226975 / правки

--- app/site/_markup/lk/accounting.php
+++ app/site/_markup/lk/accounting.php
@@ -1382,7 +1382,7 @@
                                                                                 <div class="form__field-block form__field-block--input">
                                                                                     <div class="form__control">
                                                                                         <div class="filter__select select select--mitigate select--small select--squared select--multiple" data-select>
-                                                                                            <select class="select__control" name="select2" data-select-control data-placeholder="Выбрать уровень">
+                                                                                            <select class="select__control" name="select2" data-select-control data-placeholder="Выбрать уровень" multiple>
                                                                                                 <option><!-- пустой option для placeholder --></option>
                                                                                                 <option value="1">Размещен</option>
                                                                                                 <option value="2">Отменен</option>
@@ -1414,12 +1414,14 @@
                                                                             <div class="form__field">
                                                                                 <div class="form__field-block form__field-block--input">
                                                                                     <div class="form__control">
-                                                                                        <div class="filter__sort select select--small select--sorting select--borderless" data-select>
+                                                                                        <div class="filter__sort select select--small select--sorting select--borderless select--groups" data-select>
                                                                                             <div class="select__group">
                                                                                                 <select class="select__control" name="select1" id="sort1" data-select-control data-placeholder="Сортировать по">
                                                                                                     <option><!-- пустой option для placeholder --></option>
-                                                                                                    <option value="1">По дате создания</option>
-                                                                                                    <option value="2">По сумме заказа</option>
+                                                                                                    <option value="1">По уровню консультанта</option>
+                                                                                                    <option value="2">По дате регистрации</option>
+                                                                                                    <option value="3">По количеству личных заказов со статусом “Оплачен”</option>
+                                                                                                    <option value="2">По количеству личных заказов со статусом  “Возврат”</option>
                                                                                                 </select>
                                                     
                                                                                                 <button type="button" class="input__button input__button--select button button--iconed button--covered button--square button--dark">
