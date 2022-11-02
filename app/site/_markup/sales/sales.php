da4da163b31348b38fa5a34498a8b02a048f4749
Nikolay Mamaenko <n.mamaenko@qsoft.ru>
2022-11-02 15:35:39 +0300

    226990 / Акции - Сборка страницы

---
+++ app/site/_markup/sales/sales.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Акции</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="/local/templates/.default/css/style.css" />
+    </head>
+
+    <body class="page">
+
+        <? include_once("../include/header.php"); ?>
+
+        <!--content-->
+        <div class="page__content page__content--breadcrumbs content">
+            <div class="container">
+                <main class="page__sales sales">
+                    <div class="breadcrumbs">
+                        <ul class="breadcrumbs__list">
+                            <li class="breadcrumbs__item">
+                                <a href="#" class="breadcrumbs__link">Главная</a>
+                            </li>
+                            <li class="breadcrumbs__item breadcrumbs__item--active">
+                                <a class="breadcrumbs__link">Акции</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <h1 class="page__heading">Акции</h1>
+
+                    <div class="content__main">
+                        <div class="sales__wrapper">
+                            
+                            <div class="sales__section">
+                                <div class="sales__cards cards-sale">
+                                    <ul class="cards-sale__list">
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://attuale.ru/wp-content/uploads/2018/08/maxresdefault-60.jpg"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://webpulse.imgsmail.ru/imgpreview?key=pulse_cabinet-image-12fdae35-80ca-49e9-95cc-f7bff1e0fa62&mb=webpulse"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://zooclub.ru/attach/10000/10770.jpg"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://s1.1zoom.ru/big3/70/420237-Kycb.jpg"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://zooclub.ru/attach/8107.jpg"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                        <li class="cards-sale__item">
+
+                                            <article class="card-compilation card-compilation--sale card-compilation--gigantic card-compilation--grey box box--hovering box--circle">
+                                                <a href="#" class="card-compilation__link"></a>
+                                            
+                                                <div class="card-compilation__inner">
+
+                                                    <div class="card-compilation__banner">
+                                                        <img
+                                                            src="https://lookw.ru/10/1002/1566949014-19-2.jpg"
+                                                            alt="изображение подборки"
+                                                            class="card-compilation__banner-image"
+                                                        />
+                                                    </div>
+                                                
+                                                    <div class="card-compilation__label label label--primary label--red">
+                                                        -25%
+                                                    </div>
+
+                                                    <div class="card-compilation__content">
+                                                        <p class="card-compilation__text">
+                                                            <span class="card-compilation__text-accent">
+                                                                Скидка
+                                                            </span>
+                                                            на аксессуары для собак средних и маленьких пород! Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров. 
+                                                        </p>
+                                                    </div>
+
+                                                </div>
+                                            </article>
+
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <button type="button" class="sales__button-more button button--rounded button--outlined button--green">
+                                    Показать больше
+                                </button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <!--content-->
+
+        <? include_once("../include/footer.php"); ?>
+
+        <script src="/local/templates/.default/js/script.js"></script>
+    </body>
+
+</html>
