2d664daacd54d2d5159d98a846d60589034424e9
Nikolay Mamaenko <n.mamaenko@qsoft.ru>
2022-11-02 17:30:47 +0300

    226993 / 404, 505 - Верстка страницы

---
+++ app/site/_markup/error/404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>404</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="/local/templates/.default/css/style.css" />
+    </head>
+
+    <body class="page">
+        <div class="page__error error">
+            <div class="error__content">
+                
+                <div class="error__image">
+                    <picture>
+                        <source media="(min-width: 768px)" srcset="/local/templates/.default/images/cat-404.png">
+                        <img src="/local/templates/.default/images/cat-404-mobile.png" alt="cat-404" class="error__image-picture">
+                    </picture>
+                </div>
+
+                <p class="error__status">
+                    Error 404
+                </p>
+
+                <div class="error__message">
+                    <p class="error__text">Приносим извинения, такой страницы не существует.</p>
+                    <p class="error__text">Если Вы не нашли то, что ищете, обратитесь в нашу службу поддержки. Мы постараемся Вам помочь.</p>
+                </div>
+
+                <a href="#" class="error__button button button--rounded button--covered button--red">
+                    Обратиться в техподдержку
+                </a>
+
+            </div>
+        </div>
+    </body>
+
+</html>
