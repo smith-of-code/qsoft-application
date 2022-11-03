982ebcc43a512d7e1aae9e53ed2915b03646c4f7
Александр Борисов <a.borisov@zolo-vpool>
2022-11-03 15:43:50 +0300

    Примемение новой верстки на меню.

---
+++ app/core/local/components/zolo/personal.main.profile.navigation_menu/templates/.default/lang/ru/template.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['PROFILE_TAB_NAME'] = 'Профиль';
+$MESS['HISTORY_TAB_NAME'] = 'История заказов';
+$MESS['PROFIT_TAB_NAME'] = 'Калькулятор доходности';
+$MESS['SALE_VALUE_TAB_NAME'] = 'Отчетность по объемам продаж';
+$MESS['NOTIFICATION_TAB_NAME'] = 'Уведомления';
