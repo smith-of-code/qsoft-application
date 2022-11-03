1dc1d5fce7fd76d960e5e3ecabc53fb3e232b5aa
Nikirama <n.ramenskiy@qsoft.ru>
2022-11-03 15:23:16 +0100

    Фикс после актуализации

--- app/core/local/components/zolo/personal.main.profile.navigation_menu/templates/.default/template.php
+++ app/core/local/components/zolo/personal.main.profile.navigation_menu/templates/.default/template.php
@@ -12,76 +12,62 @@ if (!defined('B_PROLOG_INCLUDED') || !B_PROLOG_INCLUDED) {
 $currentUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 ?>
 
-<div class="private__col private__col--limited">
-    <nav class="private__menu menu menu--private">
-        <ul class="menu__list">
-            <li class="menu__item <?=($arParams['PROFILE_URL'] === $currentUrl) ? 'menu__item--active' : ''?>">
-                <a href="<?=$arParams['PROFILE_URL']?>" class="menu__link">
-                    <span class="menu__icon">
-                        <svg class="icon icon--profile gui__icon">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-profile"></use>
-                        </svg>
-                    </span>
-                    <span class="menu__text">
-                        Профиль
-                    </span>
-                </a>
-            </li>
+<nav class="private__menu menu menu--private">
+    <ul class="menu__list">
+        <li class="menu__item menu__item--active">
+            <a href="/personal" class="menu__link">
+                <span class="menu__icon menu__icon--active">
+                    <svg class="icon icon--profile gui__icon">
+                        <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-profile"></use>
+                    </svg>
+                </span>
+                <span class="menu__text">Профиль</span>
+            </a>
+        </li>
 
-            <li class="menu__item <?=($arParams['ORDER_HISTORY_URL'] === $currentUrl) ? 'menu__item--active' : ''?>">
-                <a href="<?=$arParams['ORDER_HISTORY_URL']?>" class="menu__link">
-                    <span class="menu__icon">
-                        <svg class="icon icon--receipts gui__icon">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-receipts"></use>
-                        </svg>
-                    </span>
-                    <span class="menu__text">
-                        История заказов
-                    </span>
-                </a>
-            </li>
+        <li class="menu__item">
+            <a href="/personal/orders" class="menu__link">
+                <span class="menu__icon">
+                    <svg class="icon icon--receipts gui__icon">
+                        <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-receipts"></use>
+                    </svg>
+                </span>
+                <span class="menu__text">История заказов</span>
+            </a>
+        </li>
 
-            <?php if ($arParams['USER_GROUP'] == 'CONSULTANT'): ?>
-                <li class="menu__item <?=($arParams['INCOMES_CALCULATOR_URL'] === $currentUrl) ? 'menu__item--active' : ''?>">
-                    <a href="<?=$arParams['INCOMES_CALCULATOR_URL']?>" class="menu__link">
-                        <span class="menu__icon">
-                            <svg class="icon icon--calculator gui__icon">
-                                <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-calculator"></use>
-                            </svg>
-                        </span>
-                        <span class="menu__text">
-                            Калькулятор доходности
-                        </span>
-                    </a>
-                </li>
+        <li class="menu__item">
+            <a href="#" class="menu__link">
+                <span class="menu__icon">
+                    <svg class="icon icon--calculator gui__icon">
+                        <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-calculator"></use>
+                    </svg>
+                </span>
+                <span class="menu__text">Калькулятор доходности</span>
+            </a>
+        </li>
 
-                <li class="menu__item <?=($arParams['SALES_REPORT_URL'] === $currentUrl) ? 'menu__item--active' : ''?>">
-                    <a href="<?=$arParams['SALES_REPORT_URL']?>" class="menu__link">
-                        <span class="menu__icon">
-                            <svg class="icon icon--chart-square gui__icon">
-                                <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-chart-square"></use>
-                            </svg>
-                        </span>
-                        <span class="menu__text">
-                            Отчетность по объемам продаж
-                        </span>
-                    </a>
-                </li>
-            <?php endif; ?>
+        <li class="menu__item">
+            <a href="#" class="menu__link">
+                <span class="menu__icon">
+                    <svg class="icon icon--chart-square gui__icon">
+                        <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-chart-square"></use>
+                    </svg>
+                </span>
+                <span class="menu__text">Отчетность по объемам продаж</span>
+            </a>
+        </li>
 
-            <li class="menu__item <?=($arParams['NOTIFICATIONS_URL'] === $currentUrl) ? 'menu__item--active' : ''?>">
-                <a href="<?=$arParams['NOTIFICATIONS_URL']?>" class="menu__link">
-                    <span class="menu__icon">
-                        <svg class="icon icon--notification gui__icon">
-                            <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-notification"></use>
-                        </svg>
-                    </span>
-                    <span class="menu__text">
-                        Уведомления
-                    </span>
-                    <span class="menu__counter"><?=$arResult['NOTIFICATION_COUNT']?></span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
+        <li class="menu__item">
+            <a href="/personal/notifications" class="menu__link">
+                <span class="menu__icon">
+                    <svg class="icon icon--notification gui__icon">
+                        <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-notification"></use>
+                    </svg>
+                </span>
+                <span class="menu__text">Уведомления</span>
+                <span class="menu__counter">10</span>
+            </a>
+        </li>
+    </ul>
+</nav>
