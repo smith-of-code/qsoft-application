e4dd43a582d467a1df0d8d58fba41e74b0de3319
Федор Ревякин <f.revakin@qsoft.ru>
2022-11-08 11:14:27 +0300

    227042 / Миграция для создания типа и инфоблока Акции

---
+++ app/core/local/src/Migrate/2022_11_08_105059_CreateIBlockDiscounts.php
@@ -0,0 +1,62 @@
+<?php
+
+use Bitrix\Main\DB\Connection;
+use QSoft\Migrate\BaseCreateIBlockMigration;
+
+final class CreateIBlockDiscounts extends BaseCreateIBlockMigration
+{
+    protected array $iBlockInfo = [
+        'LID' => 's1',
+        'IBLOCK_TYPE_ID' => 'discounts',
+        'CODE' => 'discounts',
+        'XML_ID' => 'discounts',
+        'NAME' => 'Акции',
+        'ACTIVE' => 'Y',
+        'SORT' => 500,
+        'VERSION' => 2,
+        'RIGHTS_MODE' => 'S',
+        'API_CODE' => 'Discounts',
+        'GROUP_ID' => [
+            2 => 'R',
+            6 => 'W',
+            8 => '',
+            5 => '',
+            9 => 'W',
+            10 => '',
+            7 => '',
+            1 => 'X',
+        ],
+        'FIELDS' => [
+            'ACTIVE' => [
+                'IS_REQUIRED' => 'Y',
+                'DEFAULT_VALUE' => 'Y',
+            ],
+            'NAME' => [
+                'IS_REQUIRED' => 'Y',
+                'DEFAULT_VALUE' => '',
+            ],
+            'PREVIEW_TEXT' => [
+                'IS_REQUIRED' => 'Y',
+            ],
+            'DETAIL_PICTURE' => [
+                'IS_REQUIRED' => 'Y',
+            ],
+        ],
+    ];
+
+    protected array $iBlockPropertyInfo = [
+        [
+            'NAME' => 'Размер скидки в процентах',
+            'CODE' => 'DISCOUNT_VALUE',
+            'PROPERTY_TYPE' => 'N',
+            'IS_REQUIRED' => 'Y',
+        ],
+        [
+            'NAME' => 'Товар',
+            'CODE' => 'PRODUCT_ID',
+            'PROPERTY_TYPE' => 'E',
+            'LINK_IBLOCK_ID' => 'catalog:product',
+            'IS_REQUIRED' => 'Y',
+        ]
+    ];
+}
