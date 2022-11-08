04be04273db4ecb9bc36dfe9b8c04d4bda3edf48
Александр Борисов <a.borisov@zolo-vpool>
2022-11-08 09:48:46 +0300

    227041 / [Back] Сквозной функционал - Форма обратной связи - Заполнение заявки - Добавил компонент вывода формы в верстку.

---
+++ app/core/local/components/zolo/techsupport.form.handler/class.php
@@ -0,0 +1,49 @@
+<?
+
+use QSoft\Entity\User;
+
+ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+class TechsupportFormHandlerComponent extends CBitrixComponent
+{
+    public function onPrepareComponentParams($arParams)
+    {
+        return $arParams;
+    }
+
+	public function executeComponent()
+	{
+        if ($this->StartResultCache(false)) {
+            $this->checkRequiredModules();
+            $this->prepareResult();
+            $this->SetResultCacheKeys(["POSITION"]);
+            $this->IncludeComponentTemplate();
+        }
+	}
+    
+    protected function checkRequiredModules(): void
+    {
+        if (!CModule::IncludeModule("iblock")) {
+            ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
+        }
+        if (!CModule::IncludeModule("support")) {
+            ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
+        }
+        if (!$this->arParams["IBLOCKS"] || $this->arParams["IBLOCKS"] <= 0) {
+            ShowError(GetMessage("IBLOCK_MODULE_NOT_FOUND"));
+        }
+    }
+
+    public function prepareResult()
+    {
+        $user = new User();
+        $this->arResult['EMAIL'] = $user->email;
+        $this->arResult['ID'] = $user->id;
+        $this->arResult['ID'] = $user->id;
+    }
+
+    public function addTicket()
+    {
+        //
+    }
+}
