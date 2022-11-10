b3f3281ece2044fd2e20e828724f08bd0e7ea5a3
Федор Ревякин <f.revakin@qsoft.ru>
2022-11-10 23:51:38 +0300

    227037 / Подключение компонентов в шаблонах компонента zolo:sale.personal.orders

--- app/core/local/components/zolo/sale.personal.orders/templates/.default/detail.php
+++ app/core/local/components/zolo/sale.personal.orders/templates/.default/detail.php
@@ -1,5 +1,13 @@
-<?php if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?php
+if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+/** @var array $arResult */
 
-echo "Detail";
+$APPLICATION->IncludeComponent(
+    "zolo:sale.personal.order.detail",
+    "",
+    [
+        'ORDER_ID' => $arResult['VARIABLES']['ORDER_ID']
+    ],
+);
--- app/core/local/components/zolo/sale.personal.orders/templates/.default/list.php
+++ app/core/local/components/zolo/sale.personal.orders/templates/.default/list.php
@@ -1,5 +1,24 @@
-<?php if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?php
+if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-echo "List";
+$APPLICATION->IncludeComponent(
+    "zolo:sale.personal.order.list",
+    "",
+    [
+        "PATH_TO_CANCEL" => "/personal/order/#ID#",
+        "PATH_TO_BASKET" => "/personal/cart/",
+        "PATH_TO_COPY" => "/personal/cart/",
+        "PATH_TO_PAYMENT" => "/personal/order/payment/",
+        "ORDERS_PER_PAGE" => 5,
+        "SET_TITLE" => "Y",
+        "SAVE_IN_SESSION" => "Y",
+        "NAV_TEMPLATE" => "",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "3600",
+        "CACHE_GROUPS" => "Y",
+        "HISTORIC_STATUSES" => "F",
+        "ACTIVE_DATE_FORMAT" => "d.m.Y"
+    ]
+);
