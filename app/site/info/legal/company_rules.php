b4a8f79f94b8b65321566eb1e36972592c500f69
Федор Ревякин <f.revakin@qsoft.ru>
2022-11-17 09:44:48 +0300

    227055 / Добавление каталогов с пустыми страницами: info/legal/, info/contacts/

---
+++ app/site/info/legal/company_rules.php
@@ -0,0 +1,13 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Правила компании");
+?>
+
+<div class="section__header">
+    <h2 class="section__title">
+        Правила компании
+    </h2>
+</div>
+
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
