270bc3bc7d4d1faeb664205d6b1b000ec64383b4
Дмитрий Михайлов <d.mikhaylov@qsoft.ru>
2022-11-30 14:17:39 +0300

    Пофиксил очереди

--- app/core/local/php_interface/application.php
+++ app/core/local/php_interface/application.php
@@ -5,5 +5,24 @@ use Symfony\Component\Finder\Finder;
 $application = getApplication();
 
 foreach (Finder::create()->files()->name('*.php')->in(QSOFT_APPLICATION_ROOT . '/config') as $file) {
-    $application['config']->set(basename($file->getRealPath(), '.php'), require $file->getRealPath());
+    $application->get('config')->set(basename($file->getRealPath(), '.php'), require $file->getRealPath());
 }
+
+$application->get('config')->set('queue', [
+    'default' => 'bx',
+    'connections' => [
+        'bx' => [
+            'driver' => 'bx',
+            'queue' => 'default',
+            'retry_after' => 60,
+            'bx_connection' => 'default',
+            'bx_table' => QSoft\Queue\ORM\JobTable::class,
+        ],
+    ],
+    'failed' => [
+        'bx_connection' => 'default',
+        'bx_table' => QSoft\Queue\ORM\FailedJobTable::class,
+    ],
+]);
+
+$application->register(new \QSoft\Queue\QueueServiceProvider($application));
