e3d62c6552c789aa92f343a2ef1d305b0dfd9ee2
gn.kopylov <gn.kopylov@qsoft.ru>
2022-12-06 14:19:23 +0300

    235233 / Доработка

--- app/core/local/js/zolo/mixins/NumberFormatMixin.js
+++ app/core/local/js/zolo/mixins/NumberFormatMixin.js
@@ -5,5 +5,11 @@ export default {
             let result = parseInt(number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ');
             return useDecimals ? result : result.substring(0, result.length - 3);
         },
+
+        roundingNumber(number) {
+            if (!number) return 0;
+            let result = Math.round(parseFloat(number));
+            return result;
+        },
     },
 };
