85c5b954adb2652f4b5f0a6b461ff0dc1042848c
Nikirama <n.ramenskiy@qsoft.ru>
2022-12-26 11:33:59 +0100

    236657 / Labels integration

--- app/core/local/components/zolo/common.detail/templates/.default/template.php
+++ app/core/local/components/zolo/common.detail/templates/.default/template.php
@@ -25,7 +25,9 @@ $APPLICATION->IncludeComponent(
         </div>
 
         <div class="detail-articles__image detail-articles__image--main">
-            <div class="detail-articles__label label label--secondary label--orange">Интересное</div>
+            <div class="detail-articles__label label label--secondary label--<?=$arResult['ITEM']['MARKER_COLOR']?>">
+                <?=$arResult['ITEM']['MARKER_NAME']?>
+            </div>
             <img class="detail-articles__image-picture" src="<?=$arResult['ITEM']['DETAIL_PICTURE']?>" alt="">
         </div>
 
