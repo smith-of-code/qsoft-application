f7f1da4ba8c636dc2bfcef4cb52e783983ee647f
gn.kopylov <gn.kopylov@qsoft.ru>
2023-01-12 11:30:37 +0300

    239119 / Правки в вёрстку

--- app/core/local/components/zolo/catalog.element/templates/.default/template.php
+++ app/core/local/components/zolo/catalog.element/templates/.default/template.php
@@ -35,7 +35,11 @@ $offerId = $arResult['OFFER_FIRST'];
     prop-current-offer-id='<?= $offerId ?>'
 ></div>
 <!-- Каталог товаров -->
-<div class="content__main content__main--separated">
+<h1 class="specification__title specification__title--mobile"><?= $arResult['TITLE']?></h1><!-- Название -->
+<div id="offerArticle" prop-is-mobile="true">
+    <p class="specification__article specification__article--mobile specification__article--hidden">Арт. <?=$arResult['ARTICLES'][$offerId]?></p>
+</div>
+<div class="detail__content content__main content__main--separated">
     <div class="detail__card">
         <!--  Слайдер  в картинками ТП -->
         <div id="imageSlider" prop-is-authorized="<?= json_encode($USER->isAuthorized()) ?>" prop-no-image-placeholder="<?=NO_IMAGE_PLACEHOLDER_PATH?>">
