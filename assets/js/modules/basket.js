d803a66025410b68e4f499d061832b658ffeccc3
gn.kopylov <gn.kopylov@qsoft.ru>
2023-01-18 13:14:31 +0300

    239118 / Доработки

--- assets/js/modules/basket.js
+++ assets/js/modules/basket.js
@@ -339,12 +339,12 @@ export default async function () {
 
         let totalFixied = total.toFixed(2);
         let totalRemains = totalFixied.toString().split('.')[1];
-       
+
         if (totalRemains === "00") {
-            spanWhole.text(Math.floor(total).toLocaleString('ru-RU', {minimumFractionDigits: 0}));
+            spanWhole.text(Math.floor(totalFixied).toLocaleString('ru-RU', {minimumFractionDigits: 0}));
             spanRemains.html('&nbsp;₽');
         } else {
-            spanWhole.text(Math.floor(total).toLocaleString('ru-RU', {minimumFractionDigits: 0}) + ',');
+            spanWhole.text(Math.floor(totalFixied).toLocaleString('ru-RU', {minimumFractionDigits: 0}) + ',');
             spanRemains.html(totalRemains.toLocaleString('ru-RU', {minimumFractionDigits: 0}) + '&nbsp;₽');
         }
     }
