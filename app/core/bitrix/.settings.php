3bc89530caee4d819f20036e4350ffd3403e19ff
Andrey Lebedev <a.lebedev@qsoft.ru>
2023-02-01 19:08:18 +0300

    241745 / Добавил файлы настройки битрикса под гит

---
+++ app/core/bitrix/.settings.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined('QSOFT_CORE_LOADED')) require_once(dirname(__DIR__, 3).'/bootstrap/autoload.php');
+
+return array (
+  'utf_mode' => 
+  array (
+    'value' => true,
+    'readonly' => true,
+  ),
+  'cache_flags' => 
+  array (
+    'value' => 
+    array (
+      'config_options' => 3600.0,
+      'site_domain' => 3600.0,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' => 
+  array (
+    'value' => 
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' => 
+  array (
+    'value' => 
+    array (
+      'debug' => true,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => NULL,
+    ),
+    'readonly' => false,
+  ),
+  'connections' => 
+  array (
+    'value' => 
+    array (
+      'default' => 
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('DB_NAME'),
+        'login' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+        'options' => 2.0,
+      ),
+    ),
+    'readonly' => true,
+  ),
+  'crypto' => 
+  array (
+    'value' => 
+    array (
+      'crypto_key' => 'e3cefd8218c9bb3396abb87b87c67f43',
+    ),
+    'readonly' => true,
+  ),
+);
