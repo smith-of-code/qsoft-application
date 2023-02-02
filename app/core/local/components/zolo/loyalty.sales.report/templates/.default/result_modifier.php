4ae27558c137a27f48b0e34b3613cc276d3dbb09
Александр Борисов <a.borisov@qsoft.ru>
2023-02-02 09:37:14 +0300

    Правка кода

--- app/core/local/components/zolo/loyalty.sales.report/templates/.default/result_modifier.php
+++ app/core/local/components/zolo/loyalty.sales.report/templates/.default/result_modifier.php
@@ -14,7 +14,10 @@ foreach ($arResult['user_team'] as &$team) {
     foreach ($team as &$user) {
         if ($user['orders_report']['self']['last_order_date']) {
             $isToLongTimeNoBy
-                = (new Date($currentDate))->getDiff($user['orders_report']['self']['last_order_date'])->days > 30;
+                = (
+                    new Date($currentDate))
+                    ->getDiff($user['orders_report']['self']['last_order_date']
+                )->days > 30;
 
             if ($isToLongTimeNoBy) {
                 $user['orders_report']['self']['last_order_date'] = 'Пользователь стал менее активным';
