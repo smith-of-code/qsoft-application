e9c7af27fe4a6fe28be90f5a98ddddc41a79c7e9
Александр Борисов <a.borisov@qsoft.ru>
2023-02-02 09:33:22 +0300

    239018 / Отчетность: Участники группы. Покупатели - несоответствие ВТЗ - Реализовал вывод фразы Пользователь стал менее активным, если дата последней покупки была более 30 дней назад. - http://www.corp.qsoft.ru/bitrix/admin/ticket_edit.php?ID=239018&lang=ru

---
+++ app/core/local/components/zolo/loyalty.sales.report/templates/.default/result_modifier.php
@@ -0,0 +1,29 @@
+<?php
+
+use Bitrix\Main\Type\Date;
+
+dump($arResult['user_team']);
+$currentDate = new Date();
+$date = new Date('25.12.2015 12:30:00');
+
+$diff = (new Date($currentDate))->getDiff($date);
+
+dump($currentDate, $date, $diff->days);
+
+foreach ($arResult['user_team'] as &$team) {
+    foreach ($team as &$user) {
+        if ($user['orders_report']['self']['last_order_date']) {
+            $isToLongTimeNoBy
+                = (new Date($currentDate))->getDiff($user['orders_report']['self']['last_order_date'])->days > 30;
+
+            if ($isToLongTimeNoBy) {
+                $user['orders_report']['self']['last_order_date'] = 'Пользователь стал менее активным';
+            }
+        }
+
+        unset($user);
+    }
+
+    unset($team);
+}
+
