156b4648b4741d41ddc217484b3c9ab1bc25b96f
Andrey Kleinos <andrey.kleinos@fstravel.com>
2023-04-19 21:16:57 +0300

    privacy

--- app/site/info/plug/index.php
+++ app/site/info/plug/index.php
@@ -1,60 +1,59 @@
 <?php
 if (isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER'] === 'amestore' && $_SERVER['PHP_AUTH_PW'] === 'ieShei3u'){
-    var_dump($_SERVER['DOCUMENT_ROOT'].'/urlrewrite.php');
-    require $_SERVER['DOCUMENT_ROOT'].'/urlrewrite.php';
-    exit();
-}
+//    var_dump($_SERVER['DOCUMENT_ROOT'].'/urlrewrite.php');
+    require_once $_SERVER['DOCUMENT_ROOT'].'/urlrewrite.php';
+}else{
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $arResult = array();
-    if ($_REQUEST['email'] && filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $arResult = array();
+        if ($_REQUEST['email'] && filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)){
 
 // bitrix api include
-        require_once($_SERVER['DOCUMENT_ROOT']."/bitrix/modules/main/include/prolog_before.php");
-        CModule::IncludeModule('subscribe');
-        global $USER;
+            require_once($_SERVER['DOCUMENT_ROOT']."/bitrix/modules/main/include/prolog_before.php");
+            CModule::IncludeModule('subscribe');
+            global $USER;
 
-        // запрос всех рубрик
-        $rub = CRubric::GetList(
-            array("LID"=>"ASC","SORT"=>"ASC","NAME"=>"ASC"),
-            array("ACTIVE"=>"Y", "LID"=>LANG)
-        );
-        $arRubIDS = array();
-        while ($arRub = $rub->Fetch()){
-            $arRubIDS[] = $arRub['ID'];
-        }
+            // запрос всех рубрик
+            $rub = CRubric::GetList(
+                array("LID"=>"ASC","SORT"=>"ASC","NAME"=>"ASC"),
+                array("ACTIVE"=>"Y", "LID"=>LANG)
+            );
+            $arRubIDS = array();
+            while ($arRub = $rub->Fetch()){
+                $arRubIDS[] = $arRub['ID'];
+            }
 //        var_dump($_REQUEST);
 //        exit();
-        // формируем массив с полями для создания подписки
-        $arFields = Array(
-            "USER_ID" => ($USER->IsAuthorized() ? $USER->GetID() : false),
-            "FORMAT" => "html",
-            "EMAIL" => $_REQUEST['email'],
-            "ACTIVE" => "Y",
-            "RUB_ID" => [$_REQUEST['chanel']],
-            "SEND_CONFIRM" => 'N'
-        );
+            // формируем массив с полями для создания подписки
+            $arFields = Array(
+                "USER_ID" => ($USER->IsAuthorized() ? $USER->GetID() : false),
+                "FORMAT" => "html",
+                "EMAIL" => $_REQUEST['email'],
+                "ACTIVE" => "Y",
+                "RUB_ID" => [$_REQUEST['chanel']],
+                "SEND_CONFIRM" => 'N'
+            );
 
 
-        $subscr = new CSubscription;
+            $subscr = new CSubscription;
 
-        // создаем подписку
-        $ID = $subscr->Add($arFields);
-        if ($ID > 0){
-            $arResult['status'] = 'ok';
-        } else {
-            $arResult['status'] = 'error';
-            $arResult['msg'] = str_replace("<br>","",$subscr->LAST_ERROR);
-        }
+            // создаем подписку
+            $ID = $subscr->Add($arFields);
+            if ($ID > 0){
+                $arResult['status'] = 'ok';
+            } else {
+                $arResult['status'] = 'error';
+                $arResult['msg'] = str_replace("<br>","",$subscr->LAST_ERROR);
+            }
 
-    } else $arResult['status'] = 'error';
+        } else $arResult['status'] = 'error';
 
-    header('Content-Type: application/json; charset=utf-8',true,$arResult['status']==='error'?400:200);
-    echo json_encode($arResult,JSON_UNESCAPED_UNICODE);
+        header('Content-Type: application/json; charset=utf-8',true,$arResult['status']==='error'?400:200);
+        echo json_encode($arResult,JSON_UNESCAPED_UNICODE);
 //    header('Content-Type: application/json');
 //    echo 'json_encode($arResult,JSON_UNESCAPED_UNICODE)';
 
-}else{
+    }else{
 
 
 //var_dump($_SERVER['PLUG_PAGE']);
@@ -63,29 +62,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 //exit();
 //if (strpos($_SERVER['DOCUMENT_URI'],[''])){}
 
-    $filePath = $_SERVER["DOCUMENT_ROOT"].$_SERVER["DOCUMENT_URI"];
+        $filePath = $_SERVER["DOCUMENT_ROOT"].$_SERVER["DOCUMENT_URI"];
 //var_dump($_SERVER["DOCUMENT_ROOT"].'/info/plug/');
 //var_dump();
 //exit();
 //    var_dump($filePath);
 //    var_dump(file_exists($filePath));
 
-    if ( strpos($filePath,$_SERVER["DOCUMENT_ROOT"].'/info/plug/') !== false  &&    file_exists($filePath) && !is_dir($filePath)){
+        if ( strpos($filePath,$_SERVER["DOCUMENT_ROOT"].'/info/plug/') !== false  &&    file_exists($filePath) && !is_dir($filePath)){
 
-        $mimeType = mime_content_type($filePath);
-        if (strpos($filePath,'.css') !== false){
-            $mimeType = 'text/css';
-        }
-        if (strpos($filePath,'.svg') !== false){
-            $mimeType = 'image/svg+xml';
-        }
-        header("Content-Type: $mimeType");
-        echo file_get_contents($_SERVER["DOCUMENT_ROOT"].$_SERVER["DOCUMENT_URI"]);
-    }else{
-        $mimeType = mime_content_type($_SERVER["DOCUMENT_ROOT"].'/info/plug/index.html');
+            $mimeType = mime_content_type($filePath);
+            if (strpos($filePath,'.css') !== false){
+                $mimeType = 'text/css';
+            }
+            if (strpos($filePath,'.svg') !== false){
+                $mimeType = 'image/svg+xml';
+            }
+            header("Content-Type: $mimeType");
+            echo file_get_contents($_SERVER["DOCUMENT_ROOT"].$_SERVER["DOCUMENT_URI"]);
+        }else{
+            $mimeType = mime_content_type($_SERVER["DOCUMENT_ROOT"].'/info/plug/index.html');
 
-        header("Content-Type: $mimeType");
-        echo file_get_contents($_SERVER["DOCUMENT_ROOT"].'/info/plug/index.html');
+            header("Content-Type: $mimeType");
+            echo file_get_contents($_SERVER["DOCUMENT_ROOT"].'/info/plug/index.html');
+        }
     }
+
+
 }
+
+
 
