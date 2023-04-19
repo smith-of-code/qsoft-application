be5e3837c9f658b96ce48ee1cae6cbf2ad0fa5ab
Andrey Kleinos <andrey.kleinos@fstravel.com>
2023-04-19 10:15:32 +0300

    new favicon

--- app/core/local/templates/zolo.personal/header.php
+++ app/core/local/templates/zolo.personal/header.php
@@ -15,7 +15,26 @@ $user = new User;?>
 <html lang="ru">
 <head>
     <title><?php $APPLICATION->ShowTitle() ?></title>
+
     <link rel="shortcut icon" type="image/x-icon" href="<?= SITE_DIR ?>favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= SITE_DIR ?>apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= SITE_DIR ?>apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= SITE_DIR ?>apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= SITE_DIR ?>apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= SITE_DIR ?>apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= SITE_DIR ?>apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= SITE_DIR ?>apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= SITE_DIR ?>apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_DIR ?>apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= SITE_DIR ?>android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= SITE_DIR ?>favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= SITE_DIR ?>favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_DIR ?>favicon-16x16.png">
+    <link rel="manifest" href="<?= SITE_DIR ?>manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= SITE_DIR ?>ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
