20d67aae2ec3df64f62d6a724f80a24f71fdbaba
Andrey Kleinos <andrey.kleinos@fstravel.com>
2023-05-24 19:59:04 +0300

    socials

--- app/core/local/templates/.default/components/bitrix/menu/bottom_home_social/template.php
+++ app/core/local/templates/.default/components/bitrix/menu/bottom_home_social/template.php
@@ -9,7 +9,7 @@ if (! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 
 <?php foreach($arResult as $itemIndex => $arItem):?>
     <li class="social__item">
-        <a href="<?=htmlspecialcharsbx($arItem["LINK"])?>" class="social__link">
+        <a href="<?=htmlspecialcharsbx($arItem["LINK"])?>" class="social__link social__link-<?=$arItem['PARAMS']['MESSENGER_ICON']?>">
             <svg class="icon icon--social">
                 <use xlink:href="/local/templates/.default/images/icons/sprite.svg#icon-<?=$arItem['PARAMS']['MESSENGER_ICON']?>"></use>
             </svg>
