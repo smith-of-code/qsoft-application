5ec93479250c1afc69b250fe38d2873721ec7cb3
Andrey Kleinos <andrey.kleinos@fstravel.com>
2023-06-13 18:21:29 +0300

    en plug

--- app/site/info/plug/js/main.js
+++ app/site/info/plug/js/main.js
@@ -20,3 +20,32 @@ form.addEventListener('submit',async (event)=>{
     document.querySelector('.subscribe__left-head').innerHTML = 'Спасибо за подписку!'
     document.querySelector('.subscribe__left-text').innerHTML = 'Мы сообщим Вам о запуске проекта amestore.ru!'
 })
+
+
+
+let changeLangBtn = document.querySelectorAll('.indev__lang-item')
+
+if (localStorage.getItem('plagLang')){
+    document.querySelectorAll('.root-container').forEach(e=>{
+        if (e.dataset.lang !== localStorage.getItem('plagLang')){
+            e.classList.remove('active')
+        }else {
+            e.classList.add('active')
+        }
+
+
+    })
+}
+
+
+changeLangBtn.forEach(e=>{
+    e.addEventListener('click',(event)=>{
+        if (!event.target.classList.contains('active')){
+            document.querySelectorAll('.root-container').forEach(e=>{
+                e.classList.toggle('active')
+            })
+        }
+
+        localStorage.setItem('plagLang',event.target.dataset.lang)
+    })
+})
