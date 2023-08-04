fbe55c65eecad09e2814ce9477b1cc7b32db9b70
Andrey Kleinos <andrey.kleinos@fstravel.com>
2023-08-04 21:33:17 +0300

    fix update php

--- app/core/local/php_interface/events.php
+++ app/core/local/php_interface/events.php
@@ -63,6 +63,7 @@ $eventManager->addEventHandler('sale', '\Bitrix\Sale\Internals\Discount::OnAfter
 
 
 //$eventManager->AddEventHandler('catalog', 'OnSuccessCatalogImport1C', [Import1CEventsListener::class, 'customCatalogImportStep']);
+
 //$eventManager->AddEventHandler('iblock', 'OnBeforeIBlockElementUpdate', [Import1CEventsListener::class, 'OnBeforeIBlockElementUpdate']);
 
 //AddEventHandler("iblock", "OnBeforeIBlockPropertyAdd", Array("IblockModifer", "OnBeforeIBlockPropertyAddHandler"));
